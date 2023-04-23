f7e2583d83df96cb2c2f601c7fbdcc0096a014f8
tanersaydam <tanersaydam@gmail.com>
2023-04-23 12:25:12 +0300

    Factory Method 1

---
+++ CreationalDesignPatterns.FactoryMethod/FactoryMethod.cs
@@ -0,0 +1,43 @@
+﻿namespace CreationalDesignPatterns.FactoryMethod;
+
+public abstract class WebSite
+{
+    public abstract void CreatePages();
+}
+
+public class WebSiteEcommerce : WebSite
+{
+    public override void CreatePages()
+    {
+        //E-ticaret sayfası için özelleştirilmiş kodlar
+    }
+}
+
+public class WebSiteBlog : WebSite
+{
+    public override void CreatePages()
+    {
+        //Blog sayfası için özelleştilmiş kodlar
+    }
+}
+
+public abstract class WebSiteFactory
+{
+    public abstract WebSite CreateWebSite();
+}
+
+public class WebSiteFactoryEcommerce : WebSiteFactory
+{
+    public override WebSite CreateWebSite()
+    {
+        return new WebSiteEcommerce();
+    }
+}
+
+public class WebSiteFactoryBlog : WebSiteFactory
+{
+    public override WebSite CreateWebSite()
+    {
+        return new WebSiteBlog();
+    }
+}
---
+++ CreationalDesignPatterns.FactoryMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CreationalDesignPatterns.FactoryMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //WebSiteFactory factory = new WebSiteFactoryBlog();
+            //factory.CreateWebSite();
+
+            //WebSiteFactory factoryBlog = new WebSiteFactoryBlog();
+            //factoryBlog.CreateWebSite();
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
