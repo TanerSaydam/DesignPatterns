f89fa7236ee5d89f73d5a056e10f96ec026eb378
tanersaydam <tanersaydam@gmail.com>
2023-04-26 14:47:31 +0300

    Adapter Pattern 1

---
+++ StructuralDesignPatterns.Adapter/Adapter1/IPayment.cs
@@ -0,0 +1,85 @@
+﻿namespace StructuralDesignPatterns.Adapter.Adapter1;
+
+public interface IPayment
+{
+    bool MakePayment(decimal amount);
+}
+
+public class PaypalPayment : IPayment
+{
+    public bool MakePayment(decimal amount)
+    {
+        Console.WriteLine($"Paypal ile {amount} ₺ ödendi!");
+        return true;
+    }
+}
+
+public class Stripe
+{
+    public bool Charge(decimal amount)
+    {
+        Console.WriteLine($"Stripe ile {amount} ₺ ödendi!");
+        return true;
+    }
+}
+
+public class StripPaymentAdapter : IPayment
+{
+    private Stripe _stripe;
+
+    public StripPaymentAdapter()
+    {
+        _stripe = new();
+    }
+
+    public bool MakePayment(decimal amount)
+    {
+        return _stripe.Charge(amount);
+    }
+}
+
+public class PayU
+{
+    public bool ExecutePayment(decimal amount)
+    {
+        Console.WriteLine($"PayU ile {amount} ₺ ödendi!");
+        return true;
+    }
+}
+
+public class PayUPaymentAdapter : IPayment
+{
+    private PayU _payU;
+
+    public PayUPaymentAdapter()
+    {
+        _payU = new();
+    }
+
+    public bool MakePayment(decimal amount)
+    {
+        return _payU.ExecutePayment(amount);
+    }
+}
+
+public class ShoppingCart
+{
+    private IPayment _payment;
+
+    public ShoppingCart(IPayment payment)
+    {
+        _payment = payment;
+    }
+
+    public void CheckOut(decimal amount) 
+    {
+        if(_payment.MakePayment(amount))
+        {
+            Console.WriteLine("Ödeme işlemi başarıyla tamamlandı!");
+        }
+        else
+        {
+            Console.WriteLine("Ödeme işlemi başarısız oldu!");
+        }
+    }
+}
---
+++ StructuralDesignPatterns.Adapter/Program.cs
@@ -0,0 +1,26 @@
+﻿using StructuralDesignPatterns.Adapter.Adapter1;
+
+namespace StructuralDesignPatterns.Adapter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Paypal
+            IPayment payment1 = new PaypalPayment();
+            ShoppingCart shoppingCart1 = new ShoppingCart(payment1);
+            shoppingCart1.CheckOut(100);
+
+            //Stripe
+            IPayment payment2 = new StripPaymentAdapter();
+            bool result = payment2.MakePayment(100);
+
+            //PayU
+            IPayment payment3 = new PayUPaymentAdapter();
+            ShoppingCart shoppingCart3 = new(payment3);
+            shoppingCart3.CheckOut(100);
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
