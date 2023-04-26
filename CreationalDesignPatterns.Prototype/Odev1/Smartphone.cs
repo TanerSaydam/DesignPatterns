0f4ae3fcbc53a43a11a992d3cf4818782b428451
tanersaydam <tanersaydam@gmail.com>
2023-04-26 17:00:08 +0300

    Protoype - Ödev 1

---
+++ CreationalDesignPatterns.Prototype/Odev1/Smartphone.cs
@@ -0,0 +1,13 @@
+﻿namespace CreationalDesignPatterns.Prototype.Odev1;
+
+public class Smartphone : ICloneable
+{
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public string OperatingSystem { get; set; }
+    public int StorageCapacity { get; set; }
+    public object Clone()
+    {
+        return this.MemberwiseClone();
+    }
+}
--- CreationalDesignPatterns.Prototype/Program.cs
+++ CreationalDesignPatterns.Prototype/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace CreationalDesignPatterns.Prototype;
+﻿using CreationalDesignPatterns.Prototype.Odev1;
+using System.Xml;
+
+namespace CreationalDesignPatterns.Prototype;
 
 internal class Program
 {
@@ -11,10 +14,25 @@ internal class Program
         //var yeniAraba2 = prototip.Kopya();
 
 
-        var kitap = new Kitap { Baslik = "Sefiller", Yazar= "Victor Hugo", Icerik= "Sefiller (Fransızca: Les Misérables; Fransızca telaffuz: [le mizeʁabl(ə)]), Victor Hugo tarafından yazılan tarihi romandır. İlk olarak 1862'de yayınlandı. 19. yüzyılın en büyük eserlerinden biri olarak kabul gördü. İngilizce konuşulan ülkelerde başarısız çeviriler nedeniyle genellikle orijinal Fransız ismiyle anılır. Hikâye 1815'te başlar ve 1832'deki Paris Haziran Ayaklanması'nda son bulur. Birkaç karakterin yaşamını ve birbirleriyle alakasını ele alan roman daha çok eski mahkûm Jean Valjean'ın yaşam mücadelesi ve kefaretini ödemeye çalışmasına odaklanır.\r\n\r\nYasa ve merhametin doğasının incelendiği roman ayrıca Fransa tarihi, Paris'in mimarisi ve kentsel tasarımı, siyaset, ahlak felsefesi, antimonarşizm, adalet, din, ailevi ve romantik sevginin türleri ve doğası gibi konuları özenle ele alır.\r\n\r\nYayınlanmadan önce büyük tanıtımlar yapılan roman için büyük beklenti de oldu. Roman için birçok yorum yapıldı ama çoğu olumsuzdu. Ticari olarak ise roman sadece Fransa'da değil tüm dünyada büyük başarı yakaladı. Sefiller aralarında bir müzikal ve müzikalden uyarlanan bir filmin de bulunduğu birçok tiyatro, televizyon ve sinema eserine uyarlanarak büyük popülarite elde etti." };
-        var onizleme = new OnIizleme(kitap);
-        var onIzlemeText = onizleme.OnIzlemeSonucu();
+        //var kitap = new Kitap { Baslik = "Sefiller", Yazar= "Victor Hugo", Icerik= "Sefiller (Fransızca: Les Misérables; Fransızca telaffuz: [le mizeʁabl(ə)]), Victor Hugo tarafından yazılan tarihi romandır. İlk olarak 1862'de yayınlandı. 19. yüzyılın en büyük eserlerinden biri olarak kabul gördü. İngilizce konuşulan ülkelerde başarısız çeviriler nedeniyle genellikle orijinal Fransız ismiyle anılır. Hikâye 1815'te başlar ve 1832'deki Paris Haziran Ayaklanması'nda son bulur. Birkaç karakterin yaşamını ve birbirleriyle alakasını ele alan roman daha çok eski mahkûm Jean Valjean'ın yaşam mücadelesi ve kefaretini ödemeye çalışmasına odaklanır.\r\n\r\nYasa ve merhametin doğasının incelendiği roman ayrıca Fransa tarihi, Paris'in mimarisi ve kentsel tasarımı, siyaset, ahlak felsefesi, antimonarşizm, adalet, din, ailevi ve romantik sevginin türleri ve doğası gibi konuları özenle ele alır.\r\n\r\nYayınlanmadan önce büyük tanıtımlar yapılan roman için büyük beklenti de oldu. Roman için birçok yorum yapıldı ama çoğu olumsuzdu. Ticari olarak ise roman sadece Fransa'da değil tüm dünyada büyük başarı yakaladı. Sefiller aralarında bir müzikal ve müzikalden uyarlanan bir filmin de bulunduğu birçok tiyatro, televizyon ve sinema eserine uyarlanarak büyük popülarite elde etti." };
+        //var onizleme = new OnIizleme(kitap);
+        //var onIzlemeText = onizleme.OnIzlemeSonucu();
 
-        Console.WriteLine(onIzlemeText);
+        //Console.WriteLine(onIzlemeText);
+
+        Smartphone smartphone1 = new Smartphone
+        {
+            Brand = "ExampleBrand",
+            Model = "ExampleModel",
+            OperatingSystem = "ExampleOS",
+            StorageCapacity = 64,
+        };
+
+        Smartphone smartphone2 = (Smartphone)smartphone1.Clone();
+
+        smartphone2.StorageCapacity = 128;
+
+        Console.WriteLine($"Phone 1: {smartphone1.Brand} {smartphone1.Model} {smartphone1.OperatingSystem} {smartphone1.StorageCapacity}");
+        Console.WriteLine($"Phone 2: {smartphone2.Brand} {smartphone2.Model} {smartphone2.OperatingSystem} {smartphone2.StorageCapacity}");
     }
 }
