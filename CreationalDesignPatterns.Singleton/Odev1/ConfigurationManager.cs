43f05f34d58a8a2df6a71ec4aa0156a4eeb2b1c8
tanersaydam <tanersaydam@gmail.com>
2023-04-26 16:29:47 +0300

    Singleton - Ödev 1

---
+++ CreationalDesignPatterns.Singleton/Odev1/ConfigurationManager.cs
@@ -0,0 +1,45 @@
+﻿namespace CreationalDesignPatterns.Singleton.Odev1;
+
+public sealed class ConfigurationManager
+{
+    private static ConfigurationManager _instance;
+    private Dictionary<string, string> _settings;
+    private static object _lock = new object();
+    private ConfigurationManager()
+    {
+        _settings = new Dictionary<string, string>();
+        _settings.Add("ApiKey", "12345");
+        _settings.Add("ApiUrl", "https://wwww.tanersaydam.net");
+        _settings.Add("Timeout", "30");
+    }
+
+    public static ConfigurationManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                lock(_lock)
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new ConfigurationManager();
+                    }
+                }                   
+            }
+            return _instance;
+        }
+    }
+
+    public string GetValue(string key)
+    {
+        if(_settings.TryGetValue(key, out string value))
+        {
+            return value;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
--- CreationalDesignPatterns.Singleton/Program.cs
+++ CreationalDesignPatterns.Singleton/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace CreationalDesignPatterns.Singleton
+﻿using CreationalDesignPatterns.Singleton.Odev1;
+
+namespace CreationalDesignPatterns.Singleton
 {
     internal class Program
     {
@@ -9,11 +11,16 @@
             //Database database2 = Database.Instance; //instance türetme / örnek türetme
             //Database database3 = Database.Instance; //instance türetme / örnek türetme
 
-            Logger.Instance.Log("Bağlangıçta yapılan işlem tamamlandı!");
-            Logger.Instance.Log("Sonraki işlem tamamlandı");
-            Logger logger = Logger.Instance;
+            //Logger.Instance.Log("Bağlangıçta yapılan işlem tamamlandı!");
+            //Logger.Instance.Log("Sonraki işlem tamamlandı");
+            //Logger logger = Logger.Instance;
 
-            logger.Log("Yeni bir loglama yapıldı");
+            //logger.Log("Yeni bir loglama yapıldı");
+
+            ConfigurationManager configurationManager = ConfigurationManager.Instance;
+
+            string apiKey = configurationManager.GetValue("apiKey");
+            Console.WriteLine(apiKey);
 
             Console.WriteLine("Hello, World!");
         }
