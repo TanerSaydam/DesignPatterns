5c85cbdd61c9021ac32808231d3fe4ad0a646be1
tanersaydam <tanersaydam@gmail.com>
2023-04-27 15:22:15 +0300

    Bridge Pattern 2

---
+++ StructuralDesignPatterns.Bridge/Brdige2/IDevice.cs
@@ -0,0 +1,70 @@
+﻿namespace StructuralDesignPatterns.Bridge.Brdige2;
+
+public interface IDevice
+{
+    bool IsEnabled();
+    void Enable();
+    void Disable();
+    int GetVolume();
+    void SetVolume(int percent);
+}
+
+public class Televizyon : IDevice
+{
+    private bool _isEnabled;
+    private int _volume;
+
+    public void Disable() => _isEnabled = false;
+
+    public void Enable() => _isEnabled = true;
+
+    public int GetVolume() => _volume;
+
+    public bool IsEnabled() => _isEnabled;
+
+    public void SetVolume(int percent) => _volume  = percent;
+}
+
+
+public class Radio : IDevice
+{
+    private bool _isEnabled;
+    private int _volume;
+
+    public void Disable() => _isEnabled = false;
+
+    public void Enable() => _isEnabled = true;
+
+    public int GetVolume() => _volume;
+
+    public bool IsEnabled() => _isEnabled;
+
+    public void SetVolume(int percent) => _volume = percent;
+}
+
+public class UzakKontrol
+{
+    protected IDevice Device;
+    public UzakKontrol(IDevice device)
+    {
+        Device = device;
+    }
+
+    public void TogglePower()
+    {
+        if (Device.IsEnabled())
+            Device.Disable();
+        else
+            Device.Enable();
+    }
+
+    public void SesiArttir()
+    {
+        Device.SetVolume(Device.GetVolume() + 10);
+    }
+
+    public void SesiAzalt()
+    {
+        Device.SetVolume(Device.GetVolume() - 10);
+    }
+}
--- StructuralDesignPatterns.Bridge/Program.cs
+++ StructuralDesignPatterns.Bridge/Program.cs
@@ -1,4 +1,5 @@
-﻿using StructuralDesignPatterns.Bridge.Bridge1;
+﻿using StructuralDesignPatterns.Bridge.Brdige2;
+using StructuralDesignPatterns.Bridge.Bridge1;
 
 namespace StructuralDesignPatterns.Bridge;
 
@@ -6,15 +7,25 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        //Paypal
-        IOdemeIslemi paypal = new Paypal("kullaniciAdi", "parola");
-        OdemeIslemi odemeIslemi1 = new(paypal);
-        odemeIslemi1.Yap(500);
+        ////Paypal
+        //IOdemeIslemi paypal = new Paypal("kullaniciAdi", "parola");
+        //OdemeIslemi odemeIslemi1 = new(paypal);
+        //odemeIslemi1.Yap(500);
 
-        //Stripe
-        IOdemeIslemi stripe = new Stripe("apiKey");
-        OdemeIslemi odemeIslemi2 = new(stripe);
-        odemeIslemi2.Yap(500);
+        ////Stripe
+        //IOdemeIslemi stripe = new Stripe("apiKey");
+        //OdemeIslemi odemeIslemi2 = new(stripe);
+        //odemeIslemi2.Yap(500);
+
+        IDevice tv = new Televizyon();
+        UzakKontrol tvKontrol = new(tv);
+        tvKontrol.TogglePower(); //TV'yi açar / kapatır
+        tvKontrol.SesiArttir(); //Sesi aç
+
+        IDevice radio = new Radio();
+        UzakKontrol radioKontrol = new(radio);
+        radioKontrol.TogglePower();
+        radioKontrol.SesiArttir();
 
         Console.WriteLine("Hello, World!");
     }
