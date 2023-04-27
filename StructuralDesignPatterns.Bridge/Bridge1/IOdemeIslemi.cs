93726454e0a74cc4e1a7e108952873ecdfab04ee
tanersaydam <tanersaydam@gmail.com>
2023-04-27 14:39:09 +0300

    Bridge Pattern 1

---
+++ StructuralDesignPatterns.Bridge/Bridge1/IOdemeIslemi.cs
@@ -0,0 +1,55 @@
+﻿namespace StructuralDesignPatterns.Bridge.Bridge1;
+
+public interface IOdemeIslemi
+{
+    bool OdemeYap(decimal tutar);
+}
+
+public class Paypal : IOdemeIslemi
+{
+    private string _kullaniciAdi;
+    private string _parola;
+
+    public Paypal(string parola, string kullaniciAdi)
+    {
+        _parola = parola;
+        _kullaniciAdi = kullaniciAdi;
+    }
+
+    public bool OdemeYap(decimal tutar)
+    {
+        Console.WriteLine($"Paypal ile {tutar} ₺ tutarında ödeme yapıldı!");
+        return true; 
+    }
+}
+
+public class Stripe : IOdemeIslemi
+{
+    private string _apiAnahtari;
+
+    public Stripe(string apiAnahtari)
+    {
+        _apiAnahtari = apiAnahtari;
+    }
+
+    public bool OdemeYap(decimal tutar)
+    {
+        Console.WriteLine($"Stripe ile {tutar} ₺ tutarında ödeme yapıldı!");
+        return true;
+    }
+}
+
+public class OdemeIslemi
+{
+    private IOdemeIslemi _odemeIslemi;
+
+    public OdemeIslemi(IOdemeIslemi odemeIslemi)
+    {
+        _odemeIslemi = odemeIslemi;
+    }
+
+    public bool Yap(decimal tutar)
+    {
+        return _odemeIslemi.OdemeYap(tutar);
+    }
+}
---
+++ StructuralDesignPatterns.Bridge/Program.cs
@@ -0,0 +1,21 @@
+﻿using StructuralDesignPatterns.Bridge.Bridge1;
+
+namespace StructuralDesignPatterns.Bridge;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Paypal
+        IOdemeIslemi paypal = new Paypal("kullaniciAdi", "parola");
+        OdemeIslemi odemeIslemi1 = new(paypal);
+        odemeIslemi1.Yap(500);
+
+        //Stripe
+        IOdemeIslemi stripe = new Stripe("apiKey");
+        OdemeIslemi odemeIslemi2 = new(stripe);
+        odemeIslemi2.Yap(500);
+
+        Console.WriteLine("Hello, World!");
+    }
+}
