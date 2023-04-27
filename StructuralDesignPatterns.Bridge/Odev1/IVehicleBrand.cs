565e889ee5099eb735f3159d2c5d7f09afb92fe9
tanersaydam <tanersaydam@gmail.com>
2023-04-27 15:50:09 +0300

    Bridge Pattern - Ödev 1

---
+++ StructuralDesignPatterns.Bridge/Odev1/IVehicleBrand.cs
@@ -0,0 +1,52 @@
+﻿namespace StructuralDesignPatterns.Bridge.Odev1;
+
+public interface IVehicleBrand
+{
+    string GetBrandName();
+}
+
+public interface IVehicleModel
+{
+    string GetModelName();
+}
+
+public class Toyota : IVehicleBrand
+{
+    public string GetBrandName() => "Toyota";
+}
+
+public class BMW : IVehicleBrand
+{
+    public string GetBrandName() => "BMW";
+}
+
+public class Corolla : IVehicleModel
+{
+    public string GetModelName() => "Corolla";
+}
+
+public class X3 : IVehicleModel
+{
+    public string GetModelName() => "X3";
+}
+
+public class X5 : IVehicleModel
+{
+    public string GetModelName() => "X5";
+}
+
+public class Vehicle
+{
+    private readonly IVehicleBrand _brand;
+    private readonly IVehicleModel _model;
+    public Vehicle(IVehicleBrand brand, IVehicleModel model)
+    {
+        _brand = brand;
+        _model = model;
+    }
+
+    public string GetVehicleInfo()
+    {
+        return $"{_brand.GetBrandName()} - {_model.GetModelName()}";
+    }
+}
--- StructuralDesignPatterns.Bridge/Program.cs
+++ StructuralDesignPatterns.Bridge/Program.cs
@@ -1,5 +1,6 @@
 ﻿using StructuralDesignPatterns.Bridge.Brdige2;
 using StructuralDesignPatterns.Bridge.Bridge1;
+using StructuralDesignPatterns.Bridge.Odev1;
 
 namespace StructuralDesignPatterns.Bridge;
 
@@ -17,16 +18,26 @@ internal class Program
         //OdemeIslemi odemeIslemi2 = new(stripe);
         //odemeIslemi2.Yap(500);
 
-        IDevice tv = new Televizyon();
-        UzakKontrol tvKontrol = new(tv);
-        tvKontrol.TogglePower(); //TV'yi açar / kapatır
-        tvKontrol.SesiArttir(); //Sesi aç
+        //IDevice tv = new Televizyon();
+        //UzakKontrol tvKontrol = new(tv);
+        //tvKontrol.TogglePower(); //TV'yi açar / kapatır
+        //tvKontrol.SesiArttir(); //Sesi aç
 
-        IDevice radio = new Radio();
-        UzakKontrol radioKontrol = new(radio);
-        radioKontrol.TogglePower();
-        radioKontrol.SesiArttir();
+        //IDevice radio = new Radio();
+        //UzakKontrol radioKontrol = new(radio);
+        //radioKontrol.TogglePower();
+        //radioKontrol.SesiArttir();
 
+        IVehicleBrand toyota = new Toyota();
+        IVehicleModel corolla = new Corolla();
+        Vehicle vehicle1 = new(toyota, corolla);
+        Console.WriteLine(vehicle1.GetVehicleInfo());
+
+
+        IVehicleBrand bmw = new BMW();
+        IVehicleModel x5 = new X5();
+        Vehicle vehicle2 = new(bmw, x5);
+        Console.WriteLine(vehicle2.GetVehicleInfo());
         Console.WriteLine("Hello, World!");
     }
 }
