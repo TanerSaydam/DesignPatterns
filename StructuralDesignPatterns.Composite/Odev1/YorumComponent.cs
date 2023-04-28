b6e99f1b14fbc5940c8c1fb505868644b84f1df6
tanersaydam <tanersaydam@gmail.com>
2023-04-28 17:41:16 +0300

    Composite Pattern - Ödev 1

---
+++ StructuralDesignPatterns.Composite/Odev1/YorumComponent.cs
@@ -0,0 +1,63 @@
+﻿namespace StructuralDesignPatterns.Composite.Odev1;
+
+public abstract class YorumComponent
+{
+    public abstract void Print(int seviye);
+    public virtual void Add(YorumComponent component)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual void Remove(YorumComponent component)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Yorum : YorumComponent
+{
+    private readonly string _kullaniciAdi;
+    private readonly string _metin;
+    public Yorum(string kullaniciAdi, string metin)
+    {
+        _kullaniciAdi = kullaniciAdi;
+        _metin = metin;
+    }
+
+    public override void Print(int seviye)
+    {
+        Console.WriteLine(new string('-', seviye * 2) + $"{_kullaniciAdi}: {_metin}");
+    }
+}
+
+public class AltYorum: YorumComponent
+{
+    private readonly List<YorumComponent> _altYorumlar = new List<YorumComponent>();
+    private readonly string _kullaniciAdi;
+    private readonly string _metin;
+
+    public AltYorum(string kullaniciAdi, string metin)
+    {
+        _kullaniciAdi = kullaniciAdi;
+        _metin = metin;
+    }
+
+    public override void Print(int seviye)
+    {
+        Console.WriteLine(new string('-', seviye * 2) + $"{_kullaniciAdi}: {_metin}");
+        foreach (var yorum in _altYorumlar)
+        {
+            yorum.Print(seviye + 1);
+        }
+    }
+
+    public override void Add(YorumComponent component)
+    {
+        _altYorumlar.Add(component);
+    }
+
+    public override void Remove(YorumComponent component)
+    {
+        _altYorumlar.Remove(component);
+    }
+}
--- StructuralDesignPatterns.Composite/Program.cs
+++ StructuralDesignPatterns.Composite/Program.cs
@@ -1,5 +1,6 @@
 ﻿using StructuralDesignPatterns.Composite.Composite1;
 using StructuralDesignPatterns.Composite.Composite2;
+using StructuralDesignPatterns.Composite.Odev1;
 
 namespace StructuralDesignPatterns.Composite
 {
@@ -27,20 +28,31 @@ namespace StructuralDesignPatterns.Composite
             //Console.WriteLine("Hello, World!");
 
 
-            var personel1 = new Personel("Ali", "Muhasebe");
-            var personel2 = new Personel("Ayşe", "Satış");
+            //var personel1 = new Personel("Ali", "Muhasebe");
+            //var personel2 = new Personel("Ayşe", "Satış");
 
-            var yonetici1 = new Yonetici("Mehmet", "Muhasebe Yöneticisi");
-            yonetici1.Add(personel1);
+            //var yonetici1 = new Yonetici("Mehmet", "Muhasebe Yöneticisi");
+            //yonetici1.Add(personel1);
 
-            var yonetici2 = new Yonetici("Fatma", "Satış Yöneticisi");
-            yonetici2.Add(personel2);
+            //var yonetici2 = new Yonetici("Fatma", "Satış Yöneticisi");
+            //yonetici2.Add(personel2);
 
-            var genelMudur = new Yonetici("Osman", "Genel Müdür");
-            genelMudur.Add(yonetici1);
-            genelMudur.Add(yonetici2);
+            //var genelMudur = new Yonetici("Osman", "Genel Müdür");
+            //genelMudur.Add(yonetici1);
+            //genelMudur.Add(yonetici2);
 
-            genelMudur.Print();
+            //genelMudur.Print();
+
+            var yorum1 = new AltYorum("Ahmet", "İlginç bir konu");
+            var yorum2 = new AltYorum("Mehmet", "Ahmet'e katılıyorum");
+
+            yorum1.Add(yorum2);
+
+            var yorum3 = new AltYorum("Selda", "Mehmet'e katılıyorum");
+            yorum2.Add(yorum3);
+
+            yorum1.Print(0);
+
         }
     }
 }
