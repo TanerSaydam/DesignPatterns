4839d17259f4972c48df1d66466919020d0da415
tanersaydam <tanersaydam@gmail.com>
2023-04-28 16:43:25 +0300

    Composite Pattern 1

---
+++ StructuralDesignPatterns.Composite/Composite1/MenuComponent.cs
@@ -0,0 +1,62 @@
+﻿namespace StructuralDesignPatterns.Composite.Composite1;
+
+public abstract class MenuComponent
+{
+    public abstract void Print();
+    public virtual void Add(MenuComponent component)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual void Remove(MenuComponent component)
+    {
+        throw new NotImplementedException ();
+    }
+}
+
+public class MenuItem : MenuComponent
+{
+    private readonly string _name;
+    private readonly double _price;
+
+    public MenuItem(string name, double price)
+    {
+        _name = name;
+        _price = price;
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine($"- {_name}: ₺{_price}");
+    }
+}
+
+public class Menu : MenuComponent
+{
+    private readonly List<MenuComponent> _menuComponents = new List<MenuComponent>();
+    private readonly string _name;
+
+    public Menu(string name)
+    {
+        _name = name;
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine($"{_name}:");
+        foreach (MenuComponent menuComponent in _menuComponents)
+        {
+            menuComponent.Print();
+        }
+    }
+
+    public override void Add(MenuComponent component)
+    {
+        _menuComponents.Add(component);
+    }
+
+    public override void Remove(MenuComponent component)
+    {
+        _menuComponents.Remove(component);
+    }
+}
---
+++ StructuralDesignPatterns.Composite/Program.cs
@@ -0,0 +1,29 @@
+﻿using StructuralDesignPatterns.Composite.Composite1;
+
+namespace StructuralDesignPatterns.Composite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pizza = new MenuItem("Pizza", 12.99);
+            var salate = new MenuItem("Salata", 8.99);
+            var burger = new MenuItem("Burger", 10.99);
+
+            var mainMenu = new Menu("Ana Menü");
+            mainMenu.Add(pizza);
+            mainMenu.Add(salate);
+            mainMenu.Add(burger);
+
+            var desserMenu = new Menu("Tatlı Menüsü");
+            desserMenu.Add(new MenuItem("Dondurma", 5.99));
+            desserMenu.Add(new MenuItem("Pasta", 6.99));
+
+
+            mainMenu.Add(desserMenu);
+            mainMenu.Print();
+
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
