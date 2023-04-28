75e43c0976474dd1b79c9c459c1e4716b19c1baa
tanersaydam <tanersaydam@gmail.com>
2023-04-28 17:00:39 +0300

    Composite Pattern 2

---
+++ StructuralDesignPatterns.Composite/Composite2/Calisan.cs
@@ -0,0 +1,63 @@
+﻿namespace StructuralDesignPatterns.Composite.Composite2;
+
+public abstract class Calisan
+{
+    public abstract void Print();
+    public virtual void Add(Calisan calisan)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual void Remove(Calisan calisan)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Personel : Calisan
+{
+    private readonly string _name;
+    private readonly string _profession;
+
+    public Personel(string name, string profession)
+    {
+        _name = name;
+        _profession = profession;
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine($"{_name} - {_profession}");
+    }
+}
+
+public class Yonetici : Calisan
+{
+    private readonly List<Calisan> _calisanlar = new List<Calisan>();
+    private readonly string _name;
+    private readonly string _profession;
+    public Yonetici(string name, string profession)
+    {
+        _name = name;
+        _profession = profession;
+    }   
+
+    public override void Print()
+    {
+        Console.WriteLine($"{_name} - {_profession}");
+        foreach (var calisan in _calisanlar)
+        {
+            calisan.Print();
+        }
+    }
+
+    public override void Add(Calisan calisan)
+    {
+        _calisanlar.Add(calisan);
+    }
+
+    public override void Remove(Calisan calisan)
+    {
+        _calisanlar.Remove(calisan);
+    }
+}
--- StructuralDesignPatterns.Composite/Program.cs
+++ StructuralDesignPatterns.Composite/Program.cs
@@ -1,4 +1,5 @@
 ﻿using StructuralDesignPatterns.Composite.Composite1;
+using StructuralDesignPatterns.Composite.Composite2;
 
 namespace StructuralDesignPatterns.Composite
 {
@@ -6,24 +7,40 @@ namespace StructuralDesignPatterns.Composite
     {
         static void Main(string[] args)
         {
-            var pizza = new MenuItem("Pizza", 12.99);
-            var salate = new MenuItem("Salata", 8.99);
-            var burger = new MenuItem("Burger", 10.99);
+            //var pizza = new MenuItem("Pizza", 12.99);
+            //var salate = new MenuItem("Salata", 8.99);
+            //var burger = new MenuItem("Burger", 10.99);
 
-            var mainMenu = new Menu("Ana Menü");
-            mainMenu.Add(pizza);
-            mainMenu.Add(salate);
-            mainMenu.Add(burger);
+            //var mainMenu = new Menu("Ana Menü");
+            //mainMenu.Add(pizza);
+            //mainMenu.Add(salate);
+            //mainMenu.Add(burger);
 
-            var desserMenu = new Menu("Tatlı Menüsü");
-            desserMenu.Add(new MenuItem("Dondurma", 5.99));
-            desserMenu.Add(new MenuItem("Pasta", 6.99));
+            //var desserMenu = new Menu("Tatlı Menüsü");
+            //desserMenu.Add(new MenuItem("Dondurma", 5.99));
+            //desserMenu.Add(new MenuItem("Pasta", 6.99));
 
 
-            mainMenu.Add(desserMenu);
-            mainMenu.Print();
+            //mainMenu.Add(desserMenu);
+            //mainMenu.Print();
 
             //Console.WriteLine("Hello, World!");
+
+
+            var personel1 = new Personel("Ali", "Muhasebe");
+            var personel2 = new Personel("Ayşe", "Satış");
+
+            var yonetici1 = new Yonetici("Mehmet", "Muhasebe Yöneticisi");
+            yonetici1.Add(personel1);
+
+            var yonetici2 = new Yonetici("Fatma", "Satış Yöneticisi");
+            yonetici2.Add(personel2);
+
+            var genelMudur = new Yonetici("Osman", "Genel Müdür");
+            genelMudur.Add(yonetici1);
+            genelMudur.Add(yonetici2);
+
+            genelMudur.Print();
         }
     }
 }
