7e8f9d6534fd55a0745028095bf42aadc1701e0d
tanersaydam <tanersaydam@gmail.com>
2023-04-29 17:23:15 +0300

    Decorator Pattern 2

---
+++ StructuralDesignPatterns.Decorator/Decorator2/UIComponent.cs
@@ -0,0 +1,47 @@
+﻿namespace StructuralDesignPatterns.Decorator.Decorator2;
+
+public abstract class UIComponent
+{
+    public abstract void Display();
+}
+
+public class Button : UIComponent
+{
+    public override void Display()
+    {
+        Console.WriteLine("Buton gösterildi!");
+    }
+}
+
+public abstract class UIComponentDecorator : UIComponent
+{
+    protected UIComponent _component;
+
+    public UIComponentDecorator(UIComponent component)
+    {
+        _component = component;
+    }
+
+    public override void Display()
+    {
+        _component.Display();
+    }
+}
+
+public class BorderDecoratoer : UIComponentDecorator
+{
+    public BorderDecoratoer(UIComponent component) : base(component)
+    {
+    }
+
+    public override void Display()
+    {
+        base.Display();
+        AddBorder();
+    }
+
+    private void AddBorder()
+    {
+        Console.WriteLine("Çizgi eklendi");
+    }
+}
--- StructuralDesignPatterns.Decorator/Program.cs
+++ StructuralDesignPatterns.Decorator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using StructuralDesignPatterns.Decorator.Decorator1;
+using StructuralDesignPatterns.Decorator.Decorator2;
 
 namespace StructuralDesignPatterns.Decorator
 {
@@ -6,12 +7,18 @@ namespace StructuralDesignPatterns.Decorator
     {
         static void Main(string[] args)
         {
-            Beverage espresso = new Espresso();
-            Console.WriteLine($"{espresso.Description}: {espresso.Cost()}$");
+            //Beverage espresso = new Espresso();
+            //Console.WriteLine($"{espresso.Description}: {espresso.Cost()}$");
 
-            Beverage espressoWithMilk = new Milk(espresso);
-            Console.WriteLine($"{espressoWithMilk.Description}: {espressoWithMilk.Cost()}$");
-            Console.WriteLine("Hello, World!");
+            //Beverage espressoWithMilk = new Milk(espresso);
+            //Console.WriteLine($"{espressoWithMilk.Description}: {espressoWithMilk.Cost()}$");
+            //Console.WriteLine("Hello, World!");
+
+            UIComponent button = new Button();
+            //button.Display();
+
+            UIComponent buttonWithBorder = new BorderDecoratoer(button);
+            buttonWithBorder.Display();
         }
     }
 }
