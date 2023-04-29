34d6e16e77d02a495ff7aecc2e6c9bea53fe3ed6
tanersaydam <tanersaydam@gmail.com>
2023-04-30 02:16:44 +0300

    FlyWeight 1

---
+++ StructuralDesignPatterns.FlyWeight/FlyWeight1/IShape.cs
@@ -0,0 +1,45 @@
+﻿namespace StructuralDesignPatterns.FlyWeight.FlyWeight1;
+
+public interface IShape
+{
+    void Draw(int x, int y);
+}
+
+public class Line : IShape
+{
+    public void Draw(int x, int y)
+    {
+        Console.WriteLine($"Çizgi çizildi: ({x}, {y})");
+    }
+}
+
+public class Circle : IShape
+{
+    public void Draw(int x, int y)
+    {
+        Console.WriteLine($"Daire çizildi: ({x}, {y})");
+    }
+}
+
+public class ShapeFactory
+{
+    private Dictionary<string, IShape> _shapes = new();
+
+    public IShape GetShape(string shapeType)
+    {
+        if (!_shapes.ContainsKey(shapeType))
+        {
+            switch (shapeType)
+            {
+                case "line":
+                    _shapes.Add("line", new Line());
+                    break;
+                case "circle":
+                    _shapes.Add("circle", new Circle());
+                    break;
+            }
+        }
+
+        return _shapes[shapeType];
+    }
+}
---
+++ StructuralDesignPatterns.FlyWeight/Program.cs
@@ -0,0 +1,21 @@
+﻿using StructuralDesignPatterns.FlyWeight.FlyWeight1;
+
+namespace StructuralDesignPatterns.FlyWeight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShapeFactory factory = new();
+            IShape line1 = factory.GetShape("line");
+            line1.Draw(0,0);
+
+            IShape line2 = factory.GetShape("line");
+            line2.Draw(1, 1);
+
+
+            Console.WriteLine("Line1 ve line2 aynı nesne mi?");
+            Console.WriteLine($"{ReferenceEquals(line1,line2)}");
+        }
+    }
+}
