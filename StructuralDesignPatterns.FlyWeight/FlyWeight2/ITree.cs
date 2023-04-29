ffaa219f208a22f887ac2ab68cd3e8503699c340
tanersaydam <tanersaydam@gmail.com>
2023-04-30 02:33:45 +0300

    Flyweight Pattern 2

---
+++ StructuralDesignPatterns.FlyWeight/FlyWeight2/ITree.cs
@@ -0,0 +1,56 @@
+﻿namespace StructuralDesignPatterns.FlyWeight.FlyWeight2;
+
+public interface ITree
+{
+    void Draw(int x, int y);
+}
+
+public class Tree : ITree
+{
+    private readonly string _treeType;
+
+    public Tree(string treeType)
+    {
+        _treeType = treeType;
+    }
+
+    public void Draw(int x, int y)
+    {
+        Console.WriteLine($"Ağaç türü {_treeType}, konum ({x}, {y})");
+    }
+}
+
+public class TreeFactory
+{
+    private readonly Dictionary<string, ITree> _treeCache = new();
+
+    public ITree GetTree(string treeType)
+    {
+        if (!_treeCache.ContainsKey(treeType))
+        {
+            _treeCache[treeType] = new Tree(treeType);
+        }
+
+        return _treeCache[treeType];
+    }
+}
+
+public class Forest
+{
+    private readonly List<(ITree tree, int x, int y)> _trees = new();
+    private readonly TreeFactory _treeFactory = new();
+
+    public void PlantTree(int x, int y, string treeType)
+    {
+        ITree tree = _treeFactory.GetTree(treeType);
+        _trees.Add((tree, x, y));
+    }
+
+    public void Draw()
+    {
+        foreach (var (tree,x,y) in _trees)
+        {
+            tree.Draw(x, y);
+        }
+    }
+}
--- StructuralDesignPatterns.FlyWeight/Program.cs
+++ StructuralDesignPatterns.FlyWeight/Program.cs
@@ -1,4 +1,5 @@
 ﻿using StructuralDesignPatterns.FlyWeight.FlyWeight1;
+using StructuralDesignPatterns.FlyWeight.FlyWeight2;
 
 namespace StructuralDesignPatterns.FlyWeight
 {
@@ -6,16 +7,26 @@ namespace StructuralDesignPatterns.FlyWeight
     {
         static void Main(string[] args)
         {
-            ShapeFactory factory = new();
-            IShape line1 = factory.GetShape("line");
-            line1.Draw(0,0);
+            //ShapeFactory factory = new();
+            //IShape line1 = factory.GetShape("line");
+            //line1.Draw(0,0);
 
-            IShape line2 = factory.GetShape("line");
-            line2.Draw(1, 1);
+            //IShape line2 = factory.GetShape("line");
+            //line2.Draw(1, 1);
 
 
-            Console.WriteLine("Line1 ve line2 aynı nesne mi?");
-            Console.WriteLine($"{ReferenceEquals(line1,line2)}");
+            //Console.WriteLine("Line1 ve line2 aynı nesne mi?");
+            //Console.WriteLine($"{ReferenceEquals(line1,line2)}");
+
+            Forest forest = new();
+            forest.PlantTree(1, 1, "Çam");
+            forest.PlantTree(1, 2, "Çam");
+            forest.PlantTree(1, 3, "Çam");
+            forest.PlantTree(2, 1, "Meşe");
+            forest.PlantTree(2, 2, "Meşe");
+            forest.PlantTree(3, 1, "Kavak");
+
+            forest.Draw();
         }
     }
 }
