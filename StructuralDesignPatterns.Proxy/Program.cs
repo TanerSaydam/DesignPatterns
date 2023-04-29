7fc145671aee334dc378831f15653789684f9474
tanersaydam <tanersaydam@gmail.com>
2023-04-30 02:55:57 +0300

    Proxy Pattern 1

---
+++ StructuralDesignPatterns.Proxy/Program.cs
@@ -0,0 +1,15 @@
+﻿using StructuralDesignPatterns.Proxy.Proxy1;
+
+namespace StructuralDesignPatterns.Proxy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ISubject proxy = new Proxy1.Proxy();
+            proxy.Request();
+
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ StructuralDesignPatterns.Proxy/Proxy1/ISubject.cs
@@ -0,0 +1,30 @@
+﻿namespace StructuralDesignPatterns.Proxy.Proxy1;
+
+public interface ISubject
+{
+    void Request();
+}
+
+public class RealSubject : ISubject
+{
+    public void Request()
+    {
+        Console.WriteLine("Gerçek nesne işlemi yapıldı!");
+    }
+}
+
+public class Proxy : ISubject
+{
+    private RealSubject _realSubject;
+
+    public void Request()
+    {
+        if(_realSubject == null )
+        {
+            _realSubject = new RealSubject();
+        }
+
+        Console.WriteLine("Proxy üzerinde işlem gerçekleştiriliyor...");
+        _realSubject.Request(); 
+    }
+}
