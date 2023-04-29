c5fc7a99906de6f28f4d16aca19dcf010cab6d6c
tanersaydam <tanersaydam@gmail.com>
2023-04-30 02:47:25 +0300

    Flyweight Pattern - Ödev 1

---
+++ StructuralDesignPatterns.FlyWeight/Odev1/ITextFormat.cs
@@ -0,0 +1,42 @@
+﻿namespace StructuralDesignPatterns.FlyWeight.Odev1;
+
+public interface ITextFormat
+{
+    void AppyFormat(string text);
+}
+
+public class TextFormat : ITextFormat
+{
+    public TextFormat(string font, int size, ConsoleColor color)
+    {
+        Font = font;
+        Size = size;
+        Color = color;
+    }
+
+    public string Font { get; set; }
+    public int Size { get; set; }
+    public ConsoleColor Color { get; set; }
+
+    public void AppyFormat(string text)
+    {
+        Console.WriteLine($"Metin: '{text}', Font: {Font}, Boyut: {Size}, Renk: {Color} ");
+    }
+}
+
+public class TextFormatFactory
+{
+    private readonly Dictionary<string, TextFormat> _formatCache = new();
+
+    public ITextFormat GetFormat(string font, int size, ConsoleColor color)
+    {
+        string key = $"{font}_{size}_{color}";
+
+        if (!_formatCache.ContainsKey(key))
+        {
+            _formatCache[key] = new TextFormat(font, size, color);
+        }
+
+        return _formatCache[key];
+    }
+}
--- StructuralDesignPatterns.FlyWeight/Program.cs
+++ StructuralDesignPatterns.FlyWeight/Program.cs
@@ -1,5 +1,6 @@
 ﻿using StructuralDesignPatterns.FlyWeight.FlyWeight1;
 using StructuralDesignPatterns.FlyWeight.FlyWeight2;
+using StructuralDesignPatterns.FlyWeight.Odev1;
 
 namespace StructuralDesignPatterns.FlyWeight
 {
@@ -18,15 +19,25 @@ namespace StructuralDesignPatterns.FlyWeight
             //Console.WriteLine("Line1 ve line2 aynı nesne mi?");
             //Console.WriteLine($"{ReferenceEquals(line1,line2)}");
 
-            Forest forest = new();
-            forest.PlantTree(1, 1, "Çam");
-            forest.PlantTree(1, 2, "Çam");
-            forest.PlantTree(1, 3, "Çam");
-            forest.PlantTree(2, 1, "Meşe");
-            forest.PlantTree(2, 2, "Meşe");
-            forest.PlantTree(3, 1, "Kavak");
+            //Forest forest = new();
+            //forest.PlantTree(1, 1, "Çam");
+            //forest.PlantTree(1, 2, "Çam");
+            //forest.PlantTree(1, 3, "Çam");
+            //forest.PlantTree(2, 1, "Meşe");
+            //forest.PlantTree(2, 2, "Meşe");
+            //forest.PlantTree(3, 1, "Kavak");
 
-            forest.Draw();
+            //forest.Draw();
+
+            TextFormatFactory factory = new();
+            ITextFormat format1 = factory.GetFormat("Arial", 12, ConsoleColor.Blue);
+            format1.AppyFormat("Merhaba Dünya!");
+
+            ITextFormat format2 = factory.GetFormat("Arial", 12, ConsoleColor.Blue);
+            format2.AppyFormat("Flyweight Pattern Örneği!");
+
+            Console.WriteLine("Format1 ve format2 aynı mı?");
+            Console.WriteLine(ReferenceEquals(format1,format2));
         }
     }
 }
