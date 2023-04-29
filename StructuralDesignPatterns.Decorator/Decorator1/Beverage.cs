f71b925e87f3fb0b1185d5a9a830ebbaed65c470
tanersaydam <tanersaydam@gmail.com>
2023-04-29 15:59:54 +0300

    Decorator Pattern 1

---
+++ StructuralDesignPatterns.Decorator/Decorator1/Beverage.cs
@@ -0,0 +1,41 @@
+﻿namespace StructuralDesignPatterns.Decorator.Decorator1;
+
+public abstract class Beverage
+{
+    public abstract string Description { get; }
+    public abstract double Cost();
+}
+
+public class Espresso : Beverage
+{
+    public override string Description => "Espresso";
+
+    public override double Cost()
+    {
+        return 15.50;
+    }
+}
+
+public abstract class BeverageDecorator: Beverage
+{
+    protected Beverage _beverage;
+
+    public BeverageDecorator(Beverage beverage)
+    {
+        _beverage = beverage;
+    }
+}
+
+public class Milk : BeverageDecorator
+{
+    public Milk(Beverage beverage) : base(beverage)
+    {
+    }
+
+    public override string Description => _beverage.Description + ", Süt";
+
+    public override double Cost()
+    {
+        return _beverage.Cost() + 0.50;
+    }
+}
---
+++ StructuralDesignPatterns.Decorator/Program.cs
@@ -0,0 +1,17 @@
+﻿using StructuralDesignPatterns.Decorator.Decorator1;
+
+namespace StructuralDesignPatterns.Decorator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Beverage espresso = new Espresso();
+            Console.WriteLine($"{espresso.Description}: {espresso.Cost()}$");
+
+            Beverage espressoWithMilk = new Milk(espresso);
+            Console.WriteLine($"{espressoWithMilk.Description}: {espressoWithMilk.Cost()}$");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
