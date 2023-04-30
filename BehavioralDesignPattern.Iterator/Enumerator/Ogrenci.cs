e75803fcc9a0d95cdc9e44c4194b0b8a9154bd4f
tanersaydam <tanersaydam@gmail.com>
2023-04-30 22:20:42 +0300

    Iterator Pattern 1

---
+++ BehavioralDesignPattern.Iterator/Enumerator/Ogrenci.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+namespace BehavioralDesignPattern.Iterator.Enumerator;
+
+public class Ogrenci
+{
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+}
+
+public class OgrenciKoleksiyonu : IEnumerable<Ogrenci>
+{
+    private List<Ogrenci> _ogrenciler = new();
+
+    public void OgrenciEkle(Ogrenci ogrenci)
+    {
+        _ogrenciler.Add(ogrenci);
+    }
+
+    public IEnumerator<Ogrenci> GetEnumerator()
+    {
+        return _ogrenciler.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ BehavioralDesignPattern.Iterator/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace BehavioralDesignPattern.Iterator;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //OgrenciKoleksiyonu ogrenciKoleksiyonu = new();
+        //ogrenciKoleksiyonu.OgrenciEkle(new Ogrenci { Ad = "Taner", Soyad = "Saydam" });
+        //ogrenciKoleksiyonu.OgrenciEkle(new Ogrenci { Ad = "Tugay", Soyad = "Saydam" });
+        //ogrenciKoleksiyonu.OgrenciEkle(new Ogrenci { Ad = "Tahir", Soyad = "Saydam" });
+        //ogrenciKoleksiyonu.OgrenciEkle(new Ogrenci { Ad = "Toprak", Soyad = "Saydam" });
+
+        //IEnumerator ogrenciIterator = ogrenciKoleksiyonu.GetEnumerator();
+
+        //current 
+        //MoveNext
+        //reset
+
+        //while(ogrenciIterator.MoveNext())
+        //{
+        //    Ogrenci ogrenci = ogrenciIterator.Current as Ogrenci;
+        //    Console.WriteLine($"{ogrenci.Ad} {ogrenci.Soyad}");
+        //}
+
+        //foreach (var ogrenci in ogrenciKoleksiyonu)
+        //{
+        //    Console.WriteLine($"{ogrenci.Ad} {ogrenci.Soyad}");
+        //}            
+    }
+}
