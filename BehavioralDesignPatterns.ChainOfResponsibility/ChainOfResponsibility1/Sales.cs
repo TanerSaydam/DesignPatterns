b3bf2feff22085e84cdce2c45e5276515180fe44
tanersaydam <tanersaydam@gmail.com>
2023-04-30 14:34:47 +0300

    Chain Of Responsibility 1

---
+++ BehavioralDesignPatterns.ChainOfResponsibility/ChainOfResponsibility1/Sales.cs
@@ -0,0 +1,56 @@
+﻿namespace BehavioralDesignPatterns.ChainOfResponsibility.ChainOfResponsibility1;
+
+public class Sales
+{
+    public Sales(decimal amount)
+    {
+        Amount = amount;
+    }
+
+    public decimal Amount {  get; set; }
+    public decimal Discount { get; set; }
+    public decimal FinalDiscount { get; set; }
+}
+
+public abstract class DiscountHandler
+{
+    protected DiscountHandler nextHandler;
+
+    public void SetNextHandler(DiscountHandler handler)
+    {
+        nextHandler = handler;
+    }
+
+    public abstract void ApplyDiscount(Sales sale);
+}
+
+public class FivePercentDiscountHandler : DiscountHandler
+{
+    public override void ApplyDiscount(Sales sale)
+    {
+        if(sale.Amount >= 100 && sale.Amount < 200)
+        {
+            sale.Discount = sale.Amount * 0.05m;
+            sale.FinalDiscount = sale.Amount - sale.Discount;
+        }else if(nextHandler != null)
+        {
+            nextHandler.ApplyDiscount(sale);
+        }
+    }
+}
+
+public class TenPercentDiscountHandler: DiscountHandler
+{
+    public override void ApplyDiscount(Sales sale)
+    {
+        if(sale.Amount >= 200)
+        {
+            sale.Discount = sale.Amount * 0.1m;
+            sale.FinalDiscount = sale.Amount - sale.Discount;
+        }
+        else if (nextHandler != null)
+        {
+            nextHandler.ApplyDiscount(sale);
+        }
+    }
+}
---
+++ BehavioralDesignPatterns.ChainOfResponsibility/Program.cs
@@ -0,0 +1,25 @@
+﻿using BehavioralDesignPatterns.ChainOfResponsibility.ChainOfResponsibility1;
+
+namespace BehavioralDesignPatterns.ChainOfResponsibility
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sales sale1 = new(150);
+            Sales sale2 = new(250);
+
+            DiscountHandler handler1 = new FivePercentDiscountHandler();
+            DiscountHandler handler2 = new TenPercentDiscountHandler();
+
+            handler1.SetNextHandler(handler2);
+
+            handler1.ApplyDiscount(sale1);
+            handler1.ApplyDiscount(sale2);
+
+            Console.WriteLine($"Sale 1 Amount: {sale1.Amount} \nDiscount: {sale1.Discount} \nFinal Amount: {sale1.FinalDiscount}");
+            Console.WriteLine($"Sale 2 Amount: {sale2.Amount} \nDiscount: {sale2.Discount} \nFinal Amount: {sale2.FinalDiscount}");
+
+        }
+    }
+}
