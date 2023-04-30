f873f94194991a2db878f22bb2a8cd260c70c892
tanersaydam <tanersaydam@gmail.com>
2023-04-30 15:52:38 +0300

    Command Pattern 1

---
+++ BehavioralDesignPatterns.Command/Command1/ICommand.cs
@@ -0,0 +1,37 @@
+﻿namespace BehavioralDesignPatterns.Command.Command1;
+
+public interface ICommand
+{
+    void Execute();
+}
+
+public class PrintHelloCommand : ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("Merhaba!");
+    }
+}
+
+public class PrintGoodByeCommand : ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("Hoşça kal!");
+    }
+}
+
+public class Invoker
+{
+    private ICommand _command;
+
+    public void SetCommand(ICommand command)
+    {
+        _command = command;
+    }
+
+    public void ExecuteCommand()
+    {
+        _command.Execute();
+    }
+}
---
+++ BehavioralDesignPatterns.Command/Program.cs
@@ -0,0 +1,22 @@
+﻿using BehavioralDesignPatterns.Command.Command1;
+
+namespace BehavioralDesignPatterns.Command
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            Invoker invoker = new();
+
+            ICommand helloCommand = new PrintHelloCommand();
+            invoker.SetCommand(helloCommand);
+            invoker.ExecuteCommand();
+
+            ICommand goodByeCommand = new PrintGoodByeCommand();
+            invoker.SetCommand(goodByeCommand);
+            invoker.ExecuteCommand();
+        }
+    }
+}
