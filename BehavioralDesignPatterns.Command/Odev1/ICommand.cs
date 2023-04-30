3495471119d4198b2a6efbe66d68d999e3e32227
tanersaydam <tanersaydam@gmail.com>
2023-04-30 16:54:40 +0300

    Command Pattern - Ödev 1

---
+++ BehavioralDesignPatterns.Command/Odev1/ICommand.cs
@@ -0,0 +1,95 @@
+﻿namespace BehavioralDesignPatterns.Command.Odev1;
+
+public interface ICommand
+{
+    void Execute();
+}
+
+public class Espresso
+{
+    public void Prepare()
+    {
+        Console.WriteLine("Espresso hazırlanıyor...");
+    }
+}
+
+public class Latte
+{
+    public void Prepare()
+    {
+        Console.WriteLine("Latte hazırlanıyor...");
+    }
+}
+
+public class Cappuccino
+{
+    public void Prepare()
+    {
+        Console.WriteLine("Cappuccino hazırlanıyor...");
+    }
+}
+
+public class EspressoCommand : ICommand
+{
+    private Espresso _espresso;
+
+    public EspressoCommand(Espresso espresso)
+    {
+        _espresso = espresso;
+    }
+
+    public void Execute()
+    {
+        _espresso.Prepare();
+        Console.WriteLine("Espresso siparişi hazır!");
+    }
+}
+
+public class LatteCommand : ICommand
+{
+    private Latte _latte;
+
+    public LatteCommand(Latte latte)
+    {
+        _latte = latte;
+    }
+
+    public void Execute()
+    {
+        _latte.Prepare();
+        Console.WriteLine("Latte siparişi hazır!");
+    }
+}
+
+public class CappuccinoCommand : ICommand
+{
+    private Cappuccino _capuccino;
+
+    public CappuccinoCommand(Cappuccino capuccino)
+    {
+        _capuccino = capuccino;
+    }
+
+    public void Execute()
+    {
+        _capuccino.Prepare();
+        Console.WriteLine("Cappuccino siparişi hazır!");
+    }
+}
+
+public class Barista
+{
+    private ICommand _command;
+
+    public void SetOrder(ICommand command)
+    {
+        _command = command;
+    }
+
+    public void PrepareOrder()
+    {
+        _command.Execute();
+    }
+}
+
+
--- BehavioralDesignPatterns.Command/Program.cs
+++ BehavioralDesignPatterns.Command/Program.cs
@@ -1,5 +1,6 @@
 ﻿using BehavioralDesignPatterns.Command.Command1;
 using BehavioralDesignPatterns.Command.Command2;
+using BehavioralDesignPatterns.Command.Odev1;
 
 namespace BehavioralDesignPatterns.Command
 {
@@ -19,17 +20,42 @@ namespace BehavioralDesignPatterns.Command
             //invoker.SetCommand(goodByeCommand);
             //invoker.ExecuteCommand();
 
-            Light livingRoomLight = new();
-            RemoteControl remoteControl = new();
+            //Light livingRoomLight = new();
+            //RemoteControl remoteControl = new();
 
-            Command2.ICommand turnOnLight = new TurnOnLightCommand(livingRoomLight);
-            remoteControl.SetCommand(turnOnLight);
-            remoteControl.PressButton();
+            //Command2.ICommand turnOnLight = new TurnOnLightCommand(livingRoomLight);
+            //remoteControl.SetCommand(turnOnLight);
+            //remoteControl.PressButton();
 
-            Command2.ICommand turnOffLight = new TurnOffLightCommand(livingRoomLight);
-            remoteControl.SetCommand(turnOffLight);
-            remoteControl.PressButton();
+            //Command2.ICommand turnOffLight = new TurnOffLightCommand(livingRoomLight);
+            //remoteControl.SetCommand(turnOffLight);
+            //remoteControl.PressButton();
 
+            Barista barista = new();
+            Console.WriteLine("Kahve siparişi girin:\n 1. Espresso\n2. Latte\n3.Cappuccino");
+            int coffeType = int.Parse(Console.ReadLine());
+
+            switch (coffeType)
+            {
+                case 1:
+                    Espresso espresso = new();
+                    Odev1.ICommand espressoCommand = new EspressoCommand(espresso);
+                    barista.SetOrder(espressoCommand);
+                    break;
+                case 2:
+                    Latte latte = new();
+                    Odev1.ICommand latteCommand = new LatteCommand(latte);
+                    barista.SetOrder(latteCommand);
+                    break;
+                case 3:
+                    Cappuccino cappuccino = new();
+                    Odev1.ICommand cappuccinoCommand = new CappuccinoCommand(cappuccino);
+                    barista.SetOrder(cappuccinoCommand);
+                    break;
+            }
+            barista.PrepareOrder();
+
+            Console.ReadLine();
         }
     }
 }
