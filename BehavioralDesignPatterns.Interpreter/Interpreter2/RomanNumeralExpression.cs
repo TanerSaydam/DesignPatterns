dae61ef4d566b4f362e486e44df040c2b8544331
tanersaydam <tanersaydam@gmail.com>
2023-05-01 06:06:57 +0300

    Interpreter Pattern 2

---
+++ BehavioralDesignPatterns.Interpreter/Interpreter2/RomanNumeralExpression.cs
@@ -0,0 +1,88 @@
+﻿namespace BehavioralDesignPatterns.Interpreter.Interpreter2;
+
+public abstract class RomanNumeralExpression
+{
+    public abstract string One();
+    public abstract string Four();
+    public abstract string Five();
+    public abstract string Nine();
+    public abstract int Multiplier();
+
+    public void Interpret(Context context)
+    {
+        if (context.Input.Length == 0) return;
+
+        if (context.Input.StartsWith(Nine())) //IX
+        {
+            context.Output += (9 * Multiplier());
+            context.Input = context.Input.Substring(2);
+        }
+        else if (context.Input.StartsWith(Four()))
+        {
+            context.Output += (4 * Multiplier()); //IV
+            context.Input = context.Input.Substring(2);
+        }
+        else if (context.Input.StartsWith(Five()))
+        {
+            context.Output += (5 * Multiplier()); //V
+            context.Input = context.Input.Substring(1);
+        }
+        
+        
+        while(context.Input.StartsWith(One())) //I
+        {
+            context.Output += (1 * Multiplier());
+            context.Input = context.Input.Substring(1);
+        }
+    }
+}
+
+public class Context
+{
+    public string Input { get; set; }
+    public int Output { get; set; }
+
+    public Context(string input)
+    {
+        Input = input;
+        Output = 0;
+    }
+}
+
+public class OneExpression : RomanNumeralExpression
+{
+    public override string One() => "I";
+    public override string Four() => "IV";
+    public override string Five() => "V";
+    public override string Nine() => "IX";
+    public override int Multiplier() => 1;
+
+}
+
+public class TenExpression : RomanNumeralExpression
+{
+    public override string One() => "X";
+    public override string Four() => "XL";
+    public override string Five() => "L";
+    public override string Nine() => "XC";
+    public override int Multiplier() => 10;
+}
+
+public class HundredExpression : RomanNumeralExpression
+{
+    public override string One() => "C";
+    public override string Four() => "CD";
+    public override string Five() => "D";
+    public override string Nine() => "CM";
+    public override int Multiplier() => 100;
+}
+
+public class ThousandExpression : RomanNumeralExpression
+{
+    public override string One() => "M";
+    public override string Four() => " ";
+    public override string Five() => " ";
+    public override string Nine() => " ";
+    public override int Multiplier() => 1000;
+}
+
--- BehavioralDesignPatterns.Interpreter/Program.cs
+++ BehavioralDesignPatterns.Interpreter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BehavioralDesignPatterns.Interpreter.Interpreter1;
+using BehavioralDesignPatterns.Interpreter.Interpreter2;
 
 namespace BehavioralDesignPatterns.Interpreter
 {
@@ -6,16 +7,31 @@ namespace BehavioralDesignPatterns.Interpreter
     {
         static void Main(string[] args)
         {
-            Expression leftExpression = new NumberExpression(3);
-            Expression rightExpression = new NumberExpression(5);
-            Expression addExpression = new AddExpression(leftExpression, rightExpression);
+            //Expression leftExpression = new NumberExpression(3);
+            //Expression rightExpression = new NumberExpression(5);
+            //Expression addExpression = new AddExpression(leftExpression, rightExpression);
 
-            Context context = new Context();
-            int result = addExpression.Interpret(context);
+            //Context context = new Context();
+            //int result = addExpression.Interpret(context);
 
-            Console.WriteLine($"Sonuç: {result}");
+            //Console.WriteLine($"Sonuç: {result}");
 
+            string roman = "MCMXXVIII";
+            Interpreter2.Context context = new(roman);
+            List<RomanNumeralExpression> tree = new()
+            {
+                new ThousandExpression(),
+                new HundredExpression(),
+                new TenExpression(),
+                new OneExpression()
+            };
 
+            foreach (var exp in tree)
+            {
+                exp.Interpret(context);
+            }
+
+            Console.WriteLine($"{roman} = {context.Output}");
             Console.ReadLine();
         }
     }
