e9f3272e5470ec3373c2b0f0ea00595be2dc0b56
tanersaydam <tanersaydam@gmail.com>
2023-05-02 01:15:15 +0300

    Mediator Pattern - Ödev 1

---
+++ BehavioralDesignPatterns.Mediator/Odev1/IShoppingMalMediator.cs
@@ -0,0 +1,109 @@
+﻿namespace BehavioralDesignPatterns.Mediator.Odev1;
+
+public interface IShoppingMalMediator
+{
+    void RegisterDiscount(string storeName, string discount);
+    void RegisterEvent(string storeName, string eventIntfo);
+    List<string> GetDiscount();
+    List<string> GetEvents();
+}
+
+public class ShoppingMalMediator : IShoppingMalMediator
+{
+    private Dictionary<string, string> _discounts = new();
+    private Dictionary<string, string> _events = new();
+    
+    public List<string> GetDiscount()
+    {
+        List<string> discountList = new List<string>();
+        foreach (var discount in _discounts)
+        {
+            discountList.Add($"{discount.Key}: {discount.Value}");
+        }
+
+        return discountList;
+    }
+
+    public List<string> GetEvents()
+    {
+        List<string> eventList = new();
+        foreach (var e in _events)
+        {
+            eventList.Add($"{e.Key}: {e.Value}");
+        }
+
+        return eventList;
+    }
+
+    public void RegisterDiscount(string storeName, string discount)
+    {
+        _discounts[storeName] = discount;
+    }
+
+    public void RegisterEvent(string storeName, string eventIntfo)
+    {
+        _events[storeName] = eventIntfo;
+    }
+}
+
+public abstract class Colleague
+{
+    protected IShoppingMalMediator _mediator;
+
+    protected Colleague(IShoppingMalMediator mediator)
+    {
+        _mediator = mediator;
+    }
+}
+
+public class Store: Colleague
+{
+    public string Name { get; private set; }
+    public Store(string name, IShoppingMalMediator mediator): base(mediator)
+    {
+        Name = name;
+    }
+
+    public void AnnounceDiscount(string discount)
+    {
+        _mediator.RegisterDiscount(Name, discount);
+    }
+
+    public void AnnounceEvent(string eventInfo)
+    {
+        _mediator.RegisterEvent(Name, eventInfo);
+    }
+}
+
+public class Visitor : Colleague
+{
+    public string Name { get; private set; }
+    public Visitor(string name, IShoppingMalMediator mediator) : base(mediator)
+    {
+        Name = name;
+    }
+
+    public void CheckDiscounts()
+    {
+        Console.WriteLine($"{Name} indirimleri kontrol ediyor");
+
+        List<string> discounts = _mediator.GetDiscount();
+
+        foreach (var discount in discounts)
+        {
+            Console.WriteLine(discount);
+        }
+    }
+
+    public void CheckEvents()
+    {
+        Console.WriteLine($"{Name} etkinlikleri kontrol ediyor");
+
+        List<string> events = _mediator.GetEvents();
+
+        foreach (var e in events)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
--- BehavioralDesignPatterns.Mediator/Program.cs
+++ BehavioralDesignPatterns.Mediator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using BehavioralDesignPatterns.Mediator.Mediator1;
 using BehavioralDesignPatterns.Mediator.Mediator2;
+using BehavioralDesignPatterns.Mediator.Odev1;
 
 namespace BehavioralDesignPatterns.Mediator
 {
@@ -23,22 +24,35 @@ namespace BehavioralDesignPatterns.Mediator
             //mehmet.Send("Ahmet", "İyiyim, sen nasılsın Ahmet?");
             //ayse.Send("Ahmet", "Ahmet seni dün sinemada görmedim mi?");
 
-            IHavaTrafikKontrol atc = new HavaTrafikKontrol();
+            //IHavaTrafikKontrol atc = new HavaTrafikKontrol();
 
-            HavadakiUcak ucak1 = new("Uçak 1", atc);
-            HavadakiUcak ucak2 = new("Uçak 2", atc);
-            HavadakiUcak ucak3 = new("Uçak 3", atc);
-            HavadakiUcak ucak4 = new("Uçak 4", atc);
+            //HavadakiUcak ucak1 = new("Uçak 1", atc);
+            //HavadakiUcak ucak2 = new("Uçak 2", atc);
+            //HavadakiUcak ucak3 = new("Uçak 3", atc);
+            //HavadakiUcak ucak4 = new("Uçak 4", atc);
 
-            ucak1.InisIzniIste();
-            ucak2.InisIzniIste();
-            ucak3.InisIzniIste();
-            ucak4.InisIzniIste();
+            //ucak1.InisIzniIste();
+            //ucak2.InisIzniIste();
+            //ucak3.InisIzniIste();
+            //ucak4.InisIzniIste();
 
-            ucak1.InisiTamamladiginiBildir();
-            ucak2.InisiTamamladiginiBildir();
-            ucak3.InisiTamamladiginiBildir();
-            ucak4.InisiTamamladiginiBildir();
+            //ucak1.InisiTamamladiginiBildir();
+            //ucak2.InisiTamamladiginiBildir();
+            //ucak3.InisiTamamladiginiBildir();
+            //ucak4.InisiTamamladiginiBildir();
+
+            IShoppingMalMediator shoppingMalMediator = new ShoppingMalMediator();
+
+            Store store1 = new("Mağaza 1", shoppingMalMediator);
+            Store store2 = new("Mağaza 2", shoppingMalMediator);
+
+            Visitor visitor = new("Taner", shoppingMalMediator);
+
+            store1.AnnounceDiscount("Ayakkabılarda %50 indirim!");
+            store2.AnnounceEvent("Açılış partisine davetlisiniz!");
+
+            visitor.CheckDiscounts();
+            visitor.CheckEvents();
         }
     }
 }
