56d4f415fbee3ac8ce2be3a97df7f38214233b04
tanersaydam <tanersaydam@gmail.com>
2023-05-02 00:23:56 +0300

    Mediator Pattern 1

---
+++ BehavioralDesignPatterns.Mediator/Mediator1/IChatRoom.cs
@@ -0,0 +1,58 @@
+﻿namespace BehavioralDesignPatterns.Mediator.Mediator1;
+
+public interface IChatRoom
+{
+    void Register(User user);
+    void Send(string from, string to, string message);
+}
+
+public abstract class User
+{
+    public string Name { get; private set; }
+    public IChatRoom ChatRoom { get; set; }
+    protected User(string name)
+    {
+        Name = name;
+    }
+
+    public void Send(string to, string message)
+    {
+        ChatRoom.Send(Name, to, message);
+    }
+
+    public abstract void Receive(string from, string message);
+}
+
+public class ChatRoom : IChatRoom
+{
+    private Dictionary<string, User> _users = new();
+    public void Register(User user)
+    {
+        if (!_users.ContainsValue(user))
+        {
+            _users[user.Name] = user;
+            user.ChatRoom = this;
+        }
+    }
+
+    public void Send(string from, string to, string message)
+    {
+        if(_users.ContainsKey(from) && _users.ContainsKey(to))
+        {
+            User recepient = _users[to];
+            recepient.Receive(from, message);
+        }
+    }
+}
+
+public class Participant : User
+{
+    public Participant(string name): base(name)
+    {
+        
+    }
+    public override void Receive(string from, string message)
+    {
+        Console.WriteLine($"{Name}'e {from}'dan mesaj geldi, mesaj: {message}");
+    }
+}
---
+++ BehavioralDesignPatterns.Mediator/Program.cs
@@ -0,0 +1,26 @@
+﻿using BehavioralDesignPatterns.Mediator.Mediator1;
+
+namespace BehavioralDesignPatterns.Mediator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            IChatRoom room = new ChatRoom();
+
+            User ahmet = new Participant("Ahmet");
+            User mehmet = new Participant("Mehmet");
+            User ayse = new Participant("Ayşe");
+
+            room.Register(ahmet);
+            room.Register(mehmet);
+            room.Register(ayse);
+
+            ahmet.Send("Mehmet", "Merhaba, Mehmet. Nasılsın?");
+            mehmet.Send("Ahmet", "İyiyim, sen nasılsın Ahmet?");
+            ayse.Send("Ahmet", "Ahmet seni dün sinemada görmedim mi?");
+        }
+    }
+}
