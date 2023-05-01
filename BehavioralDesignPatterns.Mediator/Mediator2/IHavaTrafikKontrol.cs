caabd81294687947d6e8bba24ea2897ae190553a
tanersaydam <tanersaydam@gmail.com>
2023-05-02 00:59:21 +0300

    Mediator Pattern 2

---
+++ BehavioralDesignPatterns.Mediator/Mediator2/IHavaTrafikKontrol.cs
@@ -0,0 +1,72 @@
+﻿namespace BehavioralDesignPatterns.Mediator.Mediator2;
+
+public interface IHavaTrafikKontrol
+{
+    void InisIzniIste(Ucak ucak);
+    void InisiTamamladiginiBildir(Ucak ucak);
+}
+
+public abstract class Ucak
+{
+    public string Name { get; private set; }
+    protected IHavaTrafikKontrol _atc;
+    protected Ucak(string name, IHavaTrafikKontrol atc)
+    {
+        Name = name;
+        _atc = atc;
+    }
+
+    public abstract void InisIzniIste();
+    public abstract void InisiTamamladiginiBildir();
+}
+
+public class HavadakiUcak: Ucak
+{
+    public HavadakiUcak(string name, IHavaTrafikKontrol atc): base(name, atc)
+    {
+        
+    }
+
+    public override void InisiTamamladiginiBildir()
+    {
+        _atc.InisiTamamladiginiBildir(this);
+    }
+
+    public override void InisIzniIste()
+    {
+        _atc.InisIzniIste(this);
+    }
+}
+
+public class HavaTrafikKontrol : IHavaTrafikKontrol
+{
+    private Queue<Ucak> _inisYapanUcaklar = new();
+    
+    public void InisiTamamladiginiBildir(Ucak ucak)
+    {
+        if(_inisYapanUcaklar.Peek() == ucak)
+        {
+            Console.WriteLine($"{ucak.Name} inişi tamamlandı!");
+            _inisYapanUcaklar.Dequeue();
+
+            if(_inisYapanUcaklar.Count > 0)
+            {
+                Ucak siradakiUcak = _inisYapanUcaklar.Peek();
+                Console.WriteLine($"{siradakiUcak.Name} iniş iznin verildi!");
+            }
+        }
+    }
+
+    public void InisIzniIste(Ucak ucak)
+    {
+        if(_inisYapanUcaklar.Count() == 0)
+        {
+            Console.WriteLine($"{ucak.Name} iniş izni verildi!");
+            _inisYapanUcaklar.Enqueue(ucak);
+            return;
+        }
+
+        Console.WriteLine($"{ucak.Name} iniş için izin bekle!");
+        _inisYapanUcaklar.Enqueue(ucak);
+    }
+}
--- BehavioralDesignPatterns.Mediator/Program.cs
+++ BehavioralDesignPatterns.Mediator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BehavioralDesignPatterns.Mediator.Mediator1;
+using BehavioralDesignPatterns.Mediator.Mediator2;
 
 namespace BehavioralDesignPatterns.Mediator
 {
@@ -8,19 +9,36 @@ namespace BehavioralDesignPatterns.Mediator
         {
             //Console.WriteLine("Hello, World!");
 
-            IChatRoom room = new ChatRoom();
+            //IChatRoom room = new ChatRoom();
 
-            User ahmet = new Participant("Ahmet");
-            User mehmet = new Participant("Mehmet");
-            User ayse = new Participant("Ayşe");
+            //User ahmet = new Participant("Ahmet");
+            //User mehmet = new Participant("Mehmet");
+            //User ayse = new Participant("Ayşe");
 
-            room.Register(ahmet);
-            room.Register(mehmet);
-            room.Register(ayse);
+            //room.Register(ahmet);
+            //room.Register(mehmet);
+            //room.Register(ayse);
 
-            ahmet.Send("Mehmet", "Merhaba, Mehmet. Nasılsın?");
-            mehmet.Send("Ahmet", "İyiyim, sen nasılsın Ahmet?");
-            ayse.Send("Ahmet", "Ahmet seni dün sinemada görmedim mi?");
+            //ahmet.Send("Mehmet", "Merhaba, Mehmet. Nasılsın?");
+            //mehmet.Send("Ahmet", "İyiyim, sen nasılsın Ahmet?");
+            //ayse.Send("Ahmet", "Ahmet seni dün sinemada görmedim mi?");
+
+            IHavaTrafikKontrol atc = new HavaTrafikKontrol();
+
+            HavadakiUcak ucak1 = new("Uçak 1", atc);
+            HavadakiUcak ucak2 = new("Uçak 2", atc);
+            HavadakiUcak ucak3 = new("Uçak 3", atc);
+            HavadakiUcak ucak4 = new("Uçak 4", atc);
+
+            ucak1.InisIzniIste();
+            ucak2.InisIzniIste();
+            ucak3.InisIzniIste();
+            ucak4.InisIzniIste();
+
+            ucak1.InisiTamamladiginiBildir();
+            ucak2.InisiTamamladiginiBildir();
+            ucak3.InisiTamamladiginiBildir();
+            ucak4.InisiTamamladiginiBildir();
         }
     }
 }
