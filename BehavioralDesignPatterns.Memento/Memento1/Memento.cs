133badb5da623b70140767d2f46a0d9e762af03b
tanersaydam <tanersaydam@gmail.com>
2023-05-02 01:38:04 +0300

    Memento Pattern 1

---
+++ BehavioralDesignPatterns.Memento/Memento1/Memento.cs
@@ -0,0 +1,53 @@
+﻿namespace BehavioralDesignPatterns.Memento.Memento1;
+
+public class Memento
+{
+    private readonly string _state;
+
+    public Memento(string state)
+    {
+        _state = state;
+    }
+
+    public string GetState()
+    {
+        return _state;
+    }
+}
+
+public class Originator
+{
+    private string _state; //aktif //pasif
+
+    public void SetState(string state)
+    {
+        _state = state;
+        Console.WriteLine($"Durum: {state}");
+    }
+
+    public Memento SaveState()
+    {
+        return new Memento( _state );
+    }
+
+    public void RestoreState( Memento memento )
+    {
+        _state = memento.GetState();
+        Console.WriteLine($"Durum geri yüklendi: {_state}");
+    }
+}
+
+public class CareTaker
+{
+    private Memento _memento;
+
+    public void Save(Originator originator)
+    {
+        _memento = originator.SaveState();
+    }
+
+    public void Undo(Originator originator)
+    {
+        originator.RestoreState(_memento);
+    }
+}
---
+++ BehavioralDesignPatterns.Memento/Program.cs
@@ -0,0 +1,21 @@
+﻿using BehavioralDesignPatterns.Memento.Memento1;
+
+namespace BehavioralDesignPatterns.Memento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            Originator originator = new();
+            CareTaker taker = new();
+
+            originator.SetState("Aktif");
+            taker.Save(originator);
+
+            originator.SetState("Pasif");
+            taker.Undo(originator);
+        }
+    }
+}
