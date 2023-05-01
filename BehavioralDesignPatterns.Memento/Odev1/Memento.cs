e250711b4e100bbd1b3dae6dfdbccbcce3a1c991
tanersaydam <tanersaydam@gmail.com>
2023-05-02 02:27:05 +0300

    Memento Pattern - Ödev 1

---
+++ BehavioralDesignPatterns.Memento/Odev1/Memento.cs
@@ -0,0 +1,60 @@
+﻿namespace BehavioralDesignPatterns.Memento.Odev1;
+
+public class Memento
+{
+    public Memento(int health, int energy, int level)
+    {
+        Health = health;
+        Energy = energy;
+        Level = level;
+    }
+
+    public int Health { get;}
+    public int Energy { get;}
+    public int Level { get; }
+}
+
+public class Character
+{
+    public Character(int health, int energy, int level)
+    {
+        Health = health;
+        Energy = energy;
+        Level = level;
+    }
+
+    public int Health { get; set; }
+    public int Energy { get; set; }
+    public int Level { get; set; }
+
+    public Memento Save()
+    {
+        return new Memento(Health, Energy, Level);
+    }
+
+    public void Restore(Memento memento)
+    {
+        Health = memento.Health;
+        Energy = memento.Energy;
+        Level = memento.Level;
+    }
+}
+
+public class GameState
+{
+    private readonly Stack<Memento> _undoStack = new();
+
+    public void Save(Character character)
+    {
+        _undoStack.Push(character.Save());
+    }
+
+    public void Undo(Character character)
+    {
+        if(_undoStack.Count > 0)
+        {
+            Memento memento = _undoStack.Pop();
+            character.Restore(memento);
+        }
+    }
+}
--- BehavioralDesignPatterns.Memento/Program.cs
+++ BehavioralDesignPatterns.Memento/Program.cs
@@ -1,5 +1,6 @@
 ﻿using BehavioralDesignPatterns.Memento.Memento1;
 using BehavioralDesignPatterns.Memento.Memonto2;
+using BehavioralDesignPatterns.Memento.Odev1;
 
 namespace BehavioralDesignPatterns.Memento
 {
@@ -18,20 +19,35 @@ namespace BehavioralDesignPatterns.Memento
             //originator.SetState("Pasif");
             //taker.Undo(originator);
 
-            TextEditor textEditor = new TextEditor();
-            TextEditorHistory history = new TextEditorHistory();
+            //TextEditor textEditor = new TextEditor();
+            //TextEditorHistory history = new TextEditorHistory();
 
-            textEditor.SetText("Merhaba Dünya!");
-            history.Save(textEditor);
+            //textEditor.SetText("Merhaba Dünya!");
+            //history.Save(textEditor);
 
-            textEditor.SetText("Merhaba Dünya! Nasılsın");
-            history.Save(textEditor);
+            //textEditor.SetText("Merhaba Dünya! Nasılsın");
+            //history.Save(textEditor);
 
-            textEditor.SetText("Merhaba Dünya! Nasılsın? İyi misin?");
-            history.Undo(textEditor);
+            //textEditor.SetText("Merhaba Dünya! Nasılsın? İyi misin?");
+            //history.Undo(textEditor);
 
-            textEditor.SetText("Merhaba Dünya! Ne var ne yok?");
-            history.Redo(textEditor);
+            //textEditor.SetText("Merhaba Dünya! Ne var ne yok?");
+            //history.Redo(textEditor);
+
+            Character character = new(100, 50, 1);
+            GameState game = new GameState();
+
+            Console.WriteLine($"Başlangıç durumu: Sağlık: {character.Health}, Enerji: {character.Energy}, Level: {character.Level}");
+
+            game.Save(character);
+
+            character.Health -= 20;
+
+            Console.WriteLine($"Savaş sonrası durumu: Sağlık: {character.Health}, Enerji: {character.Energy}, Level: {character.Level}");
+
+            game.Undo(character);
+
+            Console.WriteLine($"Geri alınan: Sağlık: {character.Health}, Enerji: {character.Energy}, Level: {character.Level}");
         }
     }
 }
