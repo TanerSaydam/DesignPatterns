1ab894997d7b25ae2d2d3196a0a7e48d24626e34
tanersaydam <tanersaydam@gmail.com>
2023-05-01 06:37:23 +0300

    Iterpreter Pattern - Ödev 1

---
+++ BehavioralDesignPatterns.Interpreter/Odev1/Expression.cs
@@ -0,0 +1,98 @@
+﻿namespace BehavioralDesignPatterns.Interpreter.Odev1;
+
+public abstract class Expression
+{
+    public abstract void Interpret(Context context);
+}
+
+public class Context
+{
+    public string Input { get; set; }
+    public int Output { get; set; }
+}
+
+public class NumberExpression : Expression
+{
+    private readonly int _number;
+
+    public NumberExpression(int number)
+    {
+        _number = number;
+    }
+
+    public override void Interpret(Context context)
+    {
+        context.Output = _number;
+    }
+}
+
+public abstract class OperatorExpression : Expression
+{
+    protected Expression Left;
+    protected Expression Right;
+    protected OperatorExpression(Expression left, Expression right)
+    {
+        Left = left;
+        Right = right;
+    }
+}
+
+public class AddExpression : OperatorExpression
+{
+    public AddExpression(Expression left, Expression right) : base(left,right) {}
+    public override void Interpret(Context context)
+    {
+        Left.Interpret(context);
+        int leftResult = context.Output;
+
+        Right.Interpret(context);
+        int rightResult = context.Output;
+
+        context.Output = leftResult + rightResult;
+    }
+}
+
+public class SubtractExpression : OperatorExpression
+{
+    public SubtractExpression(Expression left, Expression right) : base(left, right) { }
+    public override void Interpret(Context context)
+    {
+        Left.Interpret(context);
+        int leftResult = context.Output;
+
+        Right.Interpret(context);
+        int rightResult = context.Output;
+
+        context.Output = leftResult - rightResult;
+    }
+}
+
+public class MultiplyExpression : OperatorExpression
+{
+    public MultiplyExpression(Expression left, Expression right) : base(left, right) { }
+    public override void Interpret(Context context)
+    {
+        Left.Interpret(context);
+        int leftResult = context.Output;
+
+        Right.Interpret(context);
+        int rightResult = context.Output;
+
+        context.Output = leftResult * rightResult;
+    }
+}
+
+public class DivideExpression : OperatorExpression
+{
+    public DivideExpression(Expression left, Expression right) : base(left, right) { }
+    public override void Interpret(Context context)
+    {
+        Left.Interpret(context);
+        int leftResult = context.Output;
+
+        Right.Interpret(context);
+        int rightResult = context.Output;
+
+        context.Output = leftResult / rightResult;
+    }
+}
--- BehavioralDesignPatterns.Interpreter/Program.cs
+++ BehavioralDesignPatterns.Interpreter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using BehavioralDesignPatterns.Interpreter.Interpreter1;
 using BehavioralDesignPatterns.Interpreter.Interpreter2;
+using BehavioralDesignPatterns.Interpreter.Odev1;
 
 namespace BehavioralDesignPatterns.Interpreter
 {
@@ -16,22 +17,40 @@ namespace BehavioralDesignPatterns.Interpreter
 
             //Console.WriteLine($"Sonuç: {result}");
 
-            string roman = "MCMXXVIII";
-            Interpreter2.Context context = new(roman);
-            List<RomanNumeralExpression> tree = new()
-            {
-                new ThousandExpression(),
-                new HundredExpression(),
-                new TenExpression(),
-                new OneExpression()
-            };
+            //string roman = "MCMXXVIII";
+            //Interpreter2.Context context = new(roman);
+            //List<RomanNumeralExpression> tree = new()
+            //{
+            //    new ThousandExpression(),
+            //    new HundredExpression(),
+            //    new TenExpression(),
+            //    new OneExpression()
+            //};
 
-            foreach (var exp in tree)
-            {
-                exp.Interpret(context);
-            }
+            //foreach (var exp in tree)
+            //{
+            //    exp.Interpret(context);
+            //}
 
-            Console.WriteLine($"{roman} = {context.Output}");
+            //Console.WriteLine($"{roman} = {context.Output}");
+
+            // 3 + 5 *2 - 4 /2; == 13 - 2;
+
+            Odev1.Expression tree = new Odev1.NumberExpression(3);
+            Odev1.Expression five = new Odev1.NumberExpression(5);
+            Odev1.Expression two = new Odev1.NumberExpression(2);
+            Odev1.Expression four = new Odev1.NumberExpression(4);
+
+            Odev1.Expression multiplication = new MultiplyExpression(five, two);
+            Odev1.Expression addition = new Odev1.AddExpression(tree, multiplication);
+            Odev1.Expression division = new DivideExpression(four, two);
+            Odev1.Expression subtraction = new SubtractExpression(addition, division);
+
+            Odev1.Context context = new();
+            subtraction.Interpret(context);
+
+            Console.WriteLine($"3 + 5 * 2 - 4 / 2 = {context.Output}");
+
             Console.ReadLine();
         }
     }
