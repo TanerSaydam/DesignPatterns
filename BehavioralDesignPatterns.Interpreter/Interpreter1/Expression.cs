e651ca00870a967f1f2459f9d339802c3de9ea8b
tanersaydam <tanersaydam@gmail.com>
2023-05-01 05:42:38 +0300

    Interpreter Pattern 1

---
+++ BehavioralDesignPatterns.Interpreter/Interpreter1/Expression.cs
@@ -0,0 +1,46 @@
+﻿namespace BehavioralDesignPatterns.Interpreter.Interpreter1;
+
+//Abstract Expression
+public abstract class Expression
+{
+    public abstract int Interpret(Context context);
+}
+
+public class Context
+{
+    //Dilbilgisi ve giriş verileri yazılabilir
+}
+
+//Terminal Expression
+public class NumberExpression : Expression
+{
+    private readonly int _number;
+
+    public NumberExpression(int number)
+    {
+        _number = number;
+    }
+
+    public override int Interpret(Context context)
+    {
+        return _number;
+    }
+}
+
+//Non Terminal Expression
+public class AddExpression : Expression
+{
+    private readonly Expression _leftExpression;
+    private readonly Expression _rightExpression;
+
+    public AddExpression(Expression leftExpression, Expression rightExpression)
+    {
+        _leftExpression = leftExpression;
+        _rightExpression = rightExpression;
+    }
+
+    public override int Interpret(Context context)
+    {
+        return _leftExpression.Interpret(context) + _rightExpression.Interpret(context);
+    }
+}
---
+++ BehavioralDesignPatterns.Interpreter/Program.cs
@@ -0,0 +1,22 @@
+﻿using BehavioralDesignPatterns.Interpreter.Interpreter1;
+
+namespace BehavioralDesignPatterns.Interpreter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression leftExpression = new NumberExpression(3);
+            Expression rightExpression = new NumberExpression(5);
+            Expression addExpression = new AddExpression(leftExpression, rightExpression);
+
+            Context context = new Context();
+            int result = addExpression.Interpret(context);
+
+            Console.WriteLine($"Sonuç: {result}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
