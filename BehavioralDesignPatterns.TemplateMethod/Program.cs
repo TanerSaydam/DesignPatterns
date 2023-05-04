7101423460b23a5c4bf832d177660456850b8223
tanersaydam <tanersaydam@gmail.com>
2023-05-05 01:21:04 +0300

    Template Method 1

---
+++ BehavioralDesignPatterns.TemplateMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using BehavioralDesignPatterns.TemplateMethod.TemplateMethod1;
+
+namespace BehavioralDesignPatterns.TemplateMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            Icecek cay = new Cay();
+            Icecek kahve = new Kahve();
+
+            Console.WriteLine("Çay hazırlanıyor...");
+            cay.IcecegiHazirla();
+
+            Console.WriteLine("Kahve hazırlanıyor...");
+            kahve.IcecegiHazirla();
+        }
+    }
+}
---
+++ BehavioralDesignPatterns.TemplateMethod/TemplateMethod1/Icecek.cs
@@ -0,0 +1,52 @@
+﻿namespace BehavioralDesignPatterns.TemplateMethod.TemplateMethod1;
+
+public abstract class Icecek
+{
+    public void IcecegiHazirla()
+    {
+        SuKaynat();
+        Demle();
+        BardagaDok();
+        KatkiEkle();
+    }
+
+    protected abstract void Demle();
+    protected abstract void KatkiEkle();
+    private void SuKaynat()
+    {
+        Console.WriteLine("Su kaynatılıyor"); ;
+    }
+
+    private void BardagaDok()
+    {
+        Console.WriteLine("Bardağa dökülüyor");
+    }
+}
+
+
+public class Cay : Icecek
+{
+    protected override void Demle()
+    {
+        Console.WriteLine("Çay demleniyor");
+    }
+
+    protected override void KatkiEkle()
+    {
+        Console.WriteLine("Limon ekleniyor");
+    }
+}
+
+public class Kahve : Icecek
+{
+    protected override void Demle()
+    {
+        Console.WriteLine("Kahve süzgeçten geçiriliyor");
+    }
+
+    protected override void KatkiEkle()
+    {
+        Console.WriteLine("Şeker ve süt ekleniyor");
+    }
+}
+
