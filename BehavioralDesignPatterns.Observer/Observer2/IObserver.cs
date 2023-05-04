412217f202e074a5e9954e37140521a392d5e846
tanersaydam <tanersaydam@gmail.com>
2023-05-04 22:51:51 +0300

    Observer Pattern 2

---
+++ BehavioralDesignPatterns.Observer/Observer2/IObserver.cs
@@ -0,0 +1,72 @@
+﻿namespace BehavioralDesignPatterns.Observer.Observer2;
+
+public interface IObserver
+{
+    void Update(float sicaklık, float nem);
+}
+
+public interface IObservable
+{
+    void Register(IObserver observer);
+    void UnRegister(IObserver observer);
+    void Notify();
+}
+
+public class HavaIstasyonu : IObservable
+{
+    private float _sicaklik;
+    private float _nem;
+
+    private List<IObserver> _observers = new();
+    public void Notify()
+    {
+        foreach (var observer in _observers)
+        {
+            observer.Update(_sicaklik, _nem);
+        }
+    }
+
+    public void Register(IObserver observer)
+    {
+        _observers.Add(observer);
+    }
+
+    public void UnRegister(IObserver observer)
+    {
+        _observers.Remove(observer);
+    }
+
+    public void OlcumleriAyarla(float sicaklik, float nem)
+    {
+        _sicaklik = sicaklik;
+        _nem = nem;
+        Notify();
+    }
+
+    public float GetSicaklik() => _sicaklik;
+    public float GetNem() => _nem;
+}
+
+public class MobileApp : IObserver
+{
+    public void Update(float sicaklık, float nem)
+    {
+        Console.WriteLine($"Mobile App: Sıcaklık: {sicaklık}, Nem: {nem}");
+    }
+}
+
+public class WebsiteApp : IObserver
+{
+    public void Update(float sicaklık, float nem)
+    {
+        Console.WriteLine($"Website App: Sıcaklık: {sicaklık}, Nem: {nem}");
+    }
+}
+
+public class LCDApp : IObserver
+{
+    public void Update(float sicaklık, float nem)
+    {
+        Console.WriteLine($"LCD App: Sıcaklık: {sicaklık}, Nem: {nem}");
+    }
+}
--- BehavioralDesignPatterns.Observer/Program.cs
+++ BehavioralDesignPatterns.Observer/Program.cs
@@ -1,25 +1,39 @@
 ﻿using BehavioralDesignPatterns.Observer.Observer1;
+using BehavioralDesignPatterns.Observer.Observer2;
 
-namespace BehavioralDesignPatterns.Observer
+namespace BehavioralDesignPatterns.Observer;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Observable observable = new();
+        //Observable observable = new();
 
-            Observer1.Observer observer1 = new("Observer1");
-            Observer1.Observer observer2 = new("Observer2");
-            Observer1.Observer observer3 = new("Observer3");
+        //Observer1.Observer observer1 = new("Observer1");
+        //Observer1.Observer observer2 = new("Observer2");
+        //Observer1.Observer observer3 = new("Observer3");
 
-            observable.Register(observer1);
-            observable.Register(observer2);
-            observable.Register(observer3);
+        //observable.Register(observer1);
+        //observable.Register(observer2);
+        //observable.Register(observer3);
 
-            observable.SendMessage("Hello world!");
-            
+        //observable.SendMessage("Hello world!");
 
-            //Console.WriteLine("Hello, World!");
-        }
+
+        //Console.WriteLine("Hello, World!");
+
+        HavaIstasyonu havaIstasyonu = new();
+
+        MobileApp mobileApp = new MobileApp();
+        WebsiteApp websiteApp = new WebsiteApp();
+        LCDApp LCDApp = new LCDApp();
+
+        havaIstasyonu.Register(mobileApp);
+        havaIstasyonu.Register(websiteApp);
+        havaIstasyonu.Register(LCDApp);
+
+        havaIstasyonu.OlcumleriAyarla(25, 60);
+        havaIstasyonu.UnRegister(websiteApp);
+        havaIstasyonu.OlcumleriAyarla(28, 55);
     }
 }
