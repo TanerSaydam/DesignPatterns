3d06370e16dc7f9d7a23317bb0d81a5a8d9b5c4e
tanersaydam <tanersaydam@gmail.com>
2023-05-05 00:27:14 +0300

    Strategy Pattern 1

---
+++ BehavioralDesignPatterns.Strategy/Program.cs
@@ -0,0 +1,21 @@
+﻿using BehavioralDesignPatterns.Strategy.Strategy1;
+
+namespace BehavioralDesignPatterns.Strategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            Shape shape = new(new DrawCircle());
+            shape.Draw();
+
+            shape.SetDrawStrategy(new DrawRectangle());
+            shape.Draw();
+
+            shape.SetDrawStrategy(new DrawTriangle());
+            shape.Draw();           
+        }
+    }
+}
---
+++ BehavioralDesignPatterns.Strategy/Strategy1/IDrawStrategy.cs
@@ -0,0 +1,51 @@
+﻿namespace BehavioralDesignPatterns.Strategy.Strategy1;
+
+public interface IDrawStrategy
+{
+    void Draw();
+}
+
+public class DrawCircle : IDrawStrategy
+{
+    public void Draw()
+    {
+        Console.WriteLine("Bir yuvarlak çiziliyor.");
+    }
+}
+
+public class DrawRectangle : IDrawStrategy
+{
+    public void Draw()
+    {
+        Console.WriteLine("Bir dikdörtgen çiziliyor.");
+    }
+}
+
+public class DrawTriangle : IDrawStrategy
+{
+    public void Draw()
+    {
+        Console.WriteLine("Bir üçgen çiziliyor.");
+    }
+}
+
+
+public class Shape
+{
+    private IDrawStrategy _strategy;
+
+    public Shape(IDrawStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    public void SetDrawStrategy(IDrawStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    public void Draw()
+    {
+        _strategy.Draw();
+    }
+}
