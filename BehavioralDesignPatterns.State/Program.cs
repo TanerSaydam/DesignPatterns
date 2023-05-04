24b5f23ef838bb1fdeac4361d1deae2919a469e4
tanersaydam <tanersaydam@gmail.com>
2023-05-04 23:38:13 +0300

    State Pattern 1

---
+++ BehavioralDesignPatterns.State/Program.cs
@@ -0,0 +1,21 @@
+﻿using BehavioralDesignPatterns.State.State1;
+
+namespace BehavioralDesignPatterns.State
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            IState initialState = new ConcreteStateA();
+            Context context = new(initialState);
+
+            context.Request();
+            context.Request();
+            context.Request();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BehavioralDesignPatterns.State/State1/IState.cs
@@ -0,0 +1,49 @@
+﻿namespace BehavioralDesignPatterns.State.State1;
+
+public interface IState
+{
+    void Handle(Context context);
+}
+
+public class Context
+{
+    private IState _state;
+
+    public Context(IState state)
+    {
+        _state = state;
+    }
+
+    public IState State
+    {
+        get { return _state;}
+        set
+        {
+            _state = value;
+            Console.WriteLine($"Durum değiştirildi. {_state.GetType().Name}");
+        }
+    }
+
+    public void Request()
+    {
+        _state.Handle(this);
+    }
+}
+
+public class ConcreteStateA : IState
+{
+    public void Handle(Context context)
+    {
+        Console.WriteLine("ConcreteStateA tarafından işleniyor.");
+        context.State = new ConcreteStateB();
+    }
+}
+
+public class ConcreteStateB : IState
+{
+    public void Handle(Context context)
+    {
+        Console.WriteLine("ConcreteStateB tarafından işleniyor.");
+        context.State = new ConcreteStateA();
+    }
+}
