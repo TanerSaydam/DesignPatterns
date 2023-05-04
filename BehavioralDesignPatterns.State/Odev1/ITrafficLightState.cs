854dc5feba5445dfea7780b192ccf3f8be670ac0
tanersaydam <tanersaydam@gmail.com>
2023-05-05 00:16:17 +0300

    State Pattern - Ödev 1

---
+++ BehavioralDesignPatterns.State/Odev1/ITrafficLightState.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehavioralDesignPatterns.State.Odev1
+{
+    public interface ITrafficLightState
+    {
+        void Change(TrafficLight trafficLight);
+    }
+
+    public class TrafficLight
+    {
+        private ITrafficLightState _state;
+
+        public TrafficLight(ITrafficLightState state)
+        {
+            _state = state;
+        }
+
+        public void SetState(ITrafficLightState state)
+        {
+            _state = state;
+        }
+
+        public void Change()
+        {
+            _state.Change(this);
+        }
+    }
+
+    public class RedState : ITrafficLightState
+    {
+        void ITrafficLightState.Change(TrafficLight trafficLight)
+        {
+            Console.WriteLine("Kırmızı ışık: Dur!");
+            trafficLight.SetState(new GreenState());
+        }
+    }
+
+    public class GreenState : ITrafficLightState
+    {
+        public void Change(TrafficLight trafficLight)
+        {
+            Console.WriteLine("Yeşil ışık: Geç!");
+            trafficLight.SetState(new YellowState());
+        }
+    }
+
+    public class YellowState : ITrafficLightState
+    {
+        public void Change(TrafficLight trafficLight)
+        {
+            Console.WriteLine("Sarı ışık: Bekle!");
+            trafficLight.SetState(new RedState());
+        }
+    }
+}
+
--- BehavioralDesignPatterns.State/Program.cs
+++ BehavioralDesignPatterns.State/Program.cs
@@ -1,4 +1,5 @@
-﻿using BehavioralDesignPatterns.State.State1;
+﻿using BehavioralDesignPatterns.State.Odev1;
+using BehavioralDesignPatterns.State.State1;
 using BehavioralDesignPatterns.State.State2;
 
 namespace BehavioralDesignPatterns.State
@@ -18,11 +19,19 @@ namespace BehavioralDesignPatterns.State
 
             //Console.ReadKey();
 
-            ATMMachine atmMachine = new();
-            //atmMachine.InsertCard();
-            atmMachine.EnterPin(1234);
-            atmMachine.RequestCash(1500);
-            atmMachine.EjectCard();
+            //ATMMachine atmMachine = new();
+            ////atmMachine.InsertCard();
+            //atmMachine.EnterPin(1234);
+            //atmMachine.RequestCash(1500);
+            //atmMachine.EjectCard();
+
+            TrafficLight trafficLight = new(new RedState());
+            for (int i = 0; i < 6; i++)
+            {
+                trafficLight.Change();
+                Thread.Sleep(1000);
+            }
+
             Console.ReadKey();
         }
     }
