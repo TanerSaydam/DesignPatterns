9ac87e41b2f4bc97208d1160102a1a45770ed12e
tanersaydam <tanersaydam@gmail.com>
2023-05-05 00:38:25 +0300

    Strategy Pattern 2

--- BehavioralDesignPatterns.Strategy/Program.cs
+++ BehavioralDesignPatterns.Strategy/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BehavioralDesignPatterns.Strategy.Strategy1;
+using BehavioralDesignPatterns.Strategy.Strategy2;
 
 namespace BehavioralDesignPatterns.Strategy
 {
@@ -8,14 +9,23 @@ namespace BehavioralDesignPatterns.Strategy
         {
             //Console.WriteLine("Hello, World!");
 
-            Shape shape = new(new DrawCircle());
-            shape.Draw();
+            //Shape shape = new(new DrawCircle());
+            //shape.Draw();
 
-            shape.SetDrawStrategy(new DrawRectangle());
-            shape.Draw();
+            //shape.SetDrawStrategy(new DrawRectangle());
+            //shape.Draw();
 
-            shape.SetDrawStrategy(new DrawTriangle());
-            shape.Draw();           
+            //shape.SetDrawStrategy(new DrawTriangle());
+            //shape.Draw();           
+
+            Checkout checkout = new(new CreditCartPayment());
+            checkout.Pay(100);
+
+            checkout.SetPaymentStrategy(new PaypalPayment());
+            checkout.Pay(200);
+
+            checkout.SetPaymentStrategy(new CryptoPayment());
+            checkout.Pay(300);
         }
     }
 }
---
+++ BehavioralDesignPatterns.Strategy/Strategy2/IPaymentStrategy.cs
@@ -0,0 +1,50 @@
+﻿namespace BehavioralDesignPatterns.Strategy.Strategy2;
+
+public interface IPaymentStrategy
+{
+    void Pay(decimal amount);
+}
+
+public class CreditCartPayment : IPaymentStrategy
+{
+    public void Pay(decimal amount)
+    {
+        Console.WriteLine($"{amount} ödemesi kredi kartı ile yapıldı!");
+    }
+}
+
+public class PaypalPayment : IPaymentStrategy
+{
+    public void Pay(decimal amount)
+    {
+        Console.WriteLine($"{amount} ödemesi paypal ile yapıldı!");
+    }
+}
+
+public class CryptoPayment : IPaymentStrategy
+{
+    public void Pay(decimal amount)
+    {
+        Console.WriteLine($"{amount} ödemesi kripto para ile yapıldı!");
+    }
+}
+
+public class Checkout
+{
+    private IPaymentStrategy _strategy;
+
+    public Checkout(IPaymentStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    public void SetPaymentStrategy(IPaymentStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    public void Pay(decimal amount)
+    {
+        _strategy.Pay(amount);
+    }
+}
