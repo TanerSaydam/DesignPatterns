9d61191b404d6010191d503196557fd75e24197a
tanersaydam <tanersaydam@gmail.com>
2023-05-04 21:37:18 +0300

    Observer Pattern 1

---
+++ BehavioralDesignPatterns.Observer/Observer1/IOvserver.cs
@@ -0,0 +1,57 @@
+﻿namespace BehavioralDesignPatterns.Observer.Observer1;
+
+public interface IObserver
+{
+    void Update(string message);
+}
+
+public interface IObservable
+{
+    void Register(IObserver observer);
+    void Unregister(IObserver observer);
+    void Notify();
+}
+
+public class Observable : IObservable
+{
+    private List<IObserver> _observers = new();
+    private string _message;
+    public void Notify()
+    {
+        foreach (var observer in _observers)
+        {
+            observer.Update(_message);
+        }
+    }
+
+    public void Register(IObserver observer)
+    {
+        _observers.Add(observer);
+    }
+
+    public void Unregister(IObserver observer)
+    {
+        _observers.Remove(observer);
+    }
+
+    public void SendMessage(string message)
+    {
+        _message = message;
+        Notify();
+    }
+}
+
+public class Observer : IObserver
+{
+    private string _name;
+
+    public Observer(string name)
+    {
+        _name = name;
+    }
+
+    public void Update(string message)
+    {
+        Console.WriteLine($"{_name} kişisinden şu mesaj geldi: {message}");
+    }
+}
---
+++ BehavioralDesignPatterns.Observer/Program.cs
@@ -0,0 +1,25 @@
+﻿using BehavioralDesignPatterns.Observer.Observer1;
+
+namespace BehavioralDesignPatterns.Observer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Observable observable = new();
+
+            Observer1.Observer observer1 = new("Observer1");
+            Observer1.Observer observer2 = new("Observer2");
+            Observer1.Observer observer3 = new("Observer3");
+
+            observable.Register(observer1);
+            observable.Register(observer2);
+            observable.Register(observer3);
+
+            observable.SendMessage("Hello world!");
+            
+
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
