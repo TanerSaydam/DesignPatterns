a7f3c08d1b6397eeeca1549c234e449fb3b6fb92
tanersaydam <tanersaydam@gmail.com>
2023-05-05 03:18:51 +0300

    Visitor Pattern 1

---
+++ BehavioralDesignPatterns.Visitor/Program.cs
@@ -0,0 +1,22 @@
+﻿using BehavioralDesignPatterns.Visitor.Visitor1;
+
+namespace BehavioralDesignPatterns.Visitor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            var elements = new IElement[] { new ConcreteElementA(), new ConcreteElementB() };
+            var visitor = new ConcreteVisitor();
+
+            foreach (var element in elements)
+            {
+                element.Accept(visitor);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BehavioralDesignPatterns.Visitor/Visitor1/IElement.cs
@@ -0,0 +1,52 @@
+﻿namespace BehavioralDesignPatterns.Visitor.Visitor1
+{
+    public interface IElement
+    {
+        void Accept(IVisitor visitor);
+    }
+
+    public class ConcreteElementA : IElement
+    {
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+        public string OperationA()
+        {
+            return "ConcreteElementA işlemi";
+        }
+    }
+
+    public class ConcreteElementB : IElement
+    {
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+        public string OperationB()
+        {
+            return "ConcreteElementB işlemi";
+        }
+    }
+
+    public interface IVisitor
+    {
+        void Visit(ConcreteElementA elementA);
+        void Visit(ConcreteElementB elementB);
+    }
+
+    public class ConcreteVisitor : IVisitor
+    {
+        public void Visit(ConcreteElementA elementA)
+        {
+            Console.WriteLine($"ConcreteVisitor ziyaterçisi, {elementA.OperationA()} işlemi gerçekleştiriyor.");
+        }
+
+        public void Visit(ConcreteElementB elementB)
+        {
+            Console.WriteLine($"ConcreteVisitor ziyaterçisi, {elementB.OperationB()} işlemi gerçekleştiriyor.");
+        }
+    }
+}
